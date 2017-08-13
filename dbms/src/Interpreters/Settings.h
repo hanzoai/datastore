vadimskipin/ClickHouse

Add distributed_ddl_task_timeout setting. [#CLICKHOUSE-3128]

dbms/src/Interpreters/Settings.h
@@ -286,7 +286,9 @@ struct Settings
     M(SettingBool, distributed_ddl_allow_replicated_alter, 0) \
     /** Limit on max column size in block while reading. Helps to decrease cache misses count. \
       * Should be close to L2 cache size. */ \
-    M(SettingUInt64, preferred_max_column_in_block_size_bytes, 0)
+    M(SettingUInt64, preferred_max_column_in_block_size_bytes, 0) \
+    /* Timeout for DDL query responses from all hosts in cluster. Negative value means infinite. */ \
+    M(SettingInt64, distributed_ddl_task_timeout, 120)
 
 
     /// Possible limits for query execution.

hanzoai/datastore

dbms/src/Interpreters/Settings.h
@@ -291,7 +291,9 @@ struct Settings
     /** Timeout for insert query into distributed. Setting is used only with insert_distributed_sync enabled. \
      *  Zero value means no timeout. \
      */ \
-    M(SettingUInt64, insert_distributed_timeout, 0)
+    M(SettingUInt64, insert_distributed_timeout, 0) \
+    /* Timeout for DDL query responses from all hosts in cluster. Negative value means infinite. */ \
+    M(SettingInt64, distributed_ddl_task_timeout, 120)
 
 
     /// Possible limits for query execution.