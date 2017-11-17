hanzoai/datastore

commit af0117fc1ad68a58d1eaedd4a6be6bb68b3e258d

Fixed tiny error [#CLICKHOUSE-2].

libs/libcommon/src/mremap.cpp
@@ -1,4 +1,3 @@
-
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -18,6 +17,7 @@ void * mremap(
 
     if (!(flags & MREMAP_MAYMOVE))
     {
+        errno = ENOMEM;
         return nullptr;
     }
 

commit 076ec562a4f666f0c191c38564191073414ab0e7

Fix clang error on osx regression from af0117fc1ad68a58d1eaedd4a6be6bb68b3e258d

libs/libcommon/src/mremap.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <sys/mman.h>
+#include <errno.h>
 #include <common/mremap.h>
 
 #if defined(MREMAP_FIXED)