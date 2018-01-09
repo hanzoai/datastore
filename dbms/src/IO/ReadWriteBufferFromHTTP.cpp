migue/ClickHouse

added http_*_timeout settings [#CLICKHOUSE-3440]

dbms/src/IO/ReadWriteBufferFromHTTP.cpp
@@ -27,8 +27,8 @@ namespace ErrorCodes
 ReadWriteBufferFromHTTP::ReadWriteBufferFromHTTP(const Poco::URI & uri,
     const std::string & method_,
     OutStreamCallback out_stream_callback,
-    size_t buffer_size_,
-    const HTTPTimeouts & timeouts)
+    const HTTPTimeouts & timeouts,
+    size_t buffer_size_)
     : ReadBuffer(nullptr, 0),
       uri{uri},
       method{!method_.empty() ? method_ : out_stream_callback ? Poco::Net::HTTPRequest::HTTP_POST : Poco::Net::HTTPRequest::HTTP_GET},

dbms/src/Storages/MergeTree/DataPartsExchange.cpp
@@ -155,6 +155,7 @@ MergeTreeData::MutableDataPartPtr Fetcher::fetchPart(
     const String & replica_path,
     const String & host,
     int port,
+    const HTTPTimeouts & timeouts,
     bool to_detached)
 {
     Poco::URI uri;
@@ -168,7 +169,7 @@ MergeTreeData::MutableDataPartPtr Fetcher::fetchPart(
         {"compress", "false"}
     });
 
-    ReadWriteBufferFromHTTP in{uri, Poco::Net::HTTPRequest::HTTP_POST};
+    ReadWriteBufferFromHTTP in{uri, Poco::Net::HTTPRequest::HTTP_POST, {}, timeouts};
 
     static const String TMP_PREFIX = "tmp_fetch_";
     String relative_part_path = String(to_detached ? "detached/" : "") + TMP_PREFIX + part_name;

dbms/src/Storages/MergeTree/DataPartsExchange.h
@@ -4,6 +4,7 @@
 #include <Storages/MergeTree/MergeTreeData.h>
 #include <IO/HashingWriteBuffer.h>
 #include <IO/copyData.h>
+#include <IO/HTTPTimeouts.h>
 
 
 namespace DB
@@ -51,6 +52,7 @@ public:
         const String & replica_path,
         const String & host,
         int port,
+        const HTTPTimeouts & timeouts,
         bool to_detached = false);
 
     /// You need to stop the data transfer.

hanzoai/datastore

dbms/src/IO/ReadWriteBufferFromHTTP.cpp
@@ -27,8 +27,8 @@ namespace ErrorCodes
 ReadWriteBufferFromHTTP::ReadWriteBufferFromHTTP(const Poco::URI & uri,
     const std::string & method_,
     OutStreamCallback out_stream_callback,
-    size_t buffer_size_,
-    const HTTPTimeouts & timeouts)
+    const ConnectionTimeouts & timeouts,
+    size_t buffer_size_)
     : ReadBuffer(nullptr, 0),
       uri{uri},
       method{!method_.empty() ? method_ : out_stream_callback ? Poco::Net::HTTPRequest::HTTP_POST : Poco::Net::HTTPRequest::HTTP_GET},

dbms/src/Storages/MergeTree/DataPartsExchange.cpp
@@ -155,6 +155,7 @@ MergeTreeData::MutableDataPartPtr Fetcher::fetchPart(
     const String & replica_path,
     const String & host,
     int port,
+    const ConnectionTimeouts & timeouts,
     bool to_detached)
 {
     Poco::URI uri;
@@ -168,7 +169,7 @@ MergeTreeData::MutableDataPartPtr Fetcher::fetchPart(
         {"compress", "false"}
     });
 
-    ReadWriteBufferFromHTTP in{uri, Poco::Net::HTTPRequest::HTTP_POST};
+    ReadWriteBufferFromHTTP in{uri, Poco::Net::HTTPRequest::HTTP_POST, {}, timeouts};
 
     static const String TMP_PREFIX = "tmp_fetch_";
     String relative_part_path = String(to_detached ? "detached/" : "") + TMP_PREFIX + part_name;

dbms/src/Storages/MergeTree/DataPartsExchange.h
@@ -4,6 +4,7 @@
 #include <Storages/MergeTree/MergeTreeData.h>
 #include <IO/HashingWriteBuffer.h>
 #include <IO/copyData.h>
+#include <IO/ConnectionTimeouts.h>
 
 
 namespace DB
@@ -51,6 +52,7 @@ public:
         const String & replica_path,
         const String & host,
         int port,
+        const ConnectionTimeouts & timeouts,
         bool to_detached = false);
 
     /// You need to stop the data transfer.