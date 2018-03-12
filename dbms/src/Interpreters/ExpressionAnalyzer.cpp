KostyaKent/ClickHouse

use ColumnsDescription everywhere [#CLICKHOUSE-3241]

dbms/src/Interpreters/ExpressionAnalyzer.cpp
@@ -190,7 +190,7 @@ ExpressionAnalyzer::ExpressionAnalyzer(
     }
 
     if (storage && source_columns.empty())
-        source_columns = storage->getSampleBlock().getNamesAndTypesList();
+        source_columns = storage->columns.getList();
     else
         removeDuplicateColumns(source_columns);
 
@@ -712,7 +712,7 @@ static std::shared_ptr<InterpreterSelectWithUnionQuery> interpretSubquery(
         /// get columns list for target table
         auto database_table = getDatabaseAndTableNameFromIdentifier(*table);
         const auto & storage = context.getTable(database_table.first, database_table.second);
-        const auto & columns = storage->getColumnsListNonMaterialized();
+        const auto & columns = storage->columns.getListNonMaterialized();
         select_expression_list->children.reserve(columns.size());
 
         /// manually substitute column names in place of asterisk
@@ -826,7 +826,9 @@ void ExpressionAnalyzer::addExternalStorage(ASTPtr & subquery_or_table_name_or_t
     Block sample = interpreter->getSampleBlock();
     NamesAndTypesList columns = sample.getNamesAndTypesList();
 
-    StoragePtr external_storage = StorageMemory::create(external_table_name, columns, NamesAndTypesList{}, NamesAndTypesList{}, ColumnDefaults{});
+    StoragePtr external_storage = StorageMemory::create(
+        external_table_name,
+        ColumnsDescription{columns, NamesAndTypesList{}, NamesAndTypesList{}, ColumnDefaults{}});
     external_storage->startup();
 
     /** We replace the subquery with the name of the temporary table.
@@ -1050,7 +1052,7 @@ void ExpressionAnalyzer::normalizeTreeImpl(
                 if (storage)
                 {
                     /// If we select from a table, get only not MATERIALIZED, not ALIAS columns.
-                    for (const auto & name_type : storage->getColumnsListNonMaterialized())
+                    for (const auto & name_type : storage->columns.getListNonMaterialized())
                         all_columns.emplace_back(std::make_shared<ASTIdentifier>(name_type.name));
                 }
                 else
@@ -1147,7 +1149,7 @@ void ExpressionAnalyzer::addAliasColumns()
     if (!storage)
         return;
 
-    source_columns.insert(std::end(source_columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
+    source_columns.insert(std::end(source_columns), std::begin(storage->columns.alias), std::end(storage->columns.alias));
 }
 
 

dbms/src/Storages/StorageDistributed.cpp
@@ -130,24 +130,21 @@ StorageDistributed::~StorageDistributed() = default;
 
 
 StorageDistributed::StorageDistributed(
-    const std::string & name_,
-    const NamesAndTypesList & columns_,
-    const NamesAndTypesList & materialized_columns_,
-    const NamesAndTypesList & alias_columns_,
-    const ColumnDefaults & column_defaults_,
+    const std::string & table_name_,
+    const ColumnsDescription & columns_,
     const String & remote_database_,
     const String & remote_table_,
     const String & cluster_name_,
     const Context & context_,
     const ASTPtr & sharding_key_,
     const String & data_path_)
-    : IStorage{columns_, materialized_columns_, alias_columns_, column_defaults_},
-    name(name_),
+    : IStorage{columns_},
+    table_name(table_name_),
     remote_database(remote_database_), remote_table(remote_table_),
     context(context_), cluster_name(context.getMacros().expand(cluster_name_)), has_sharding_key(sharding_key_),
-    sharding_key_expr(sharding_key_ ? ExpressionAnalyzer(sharding_key_, context, nullptr, columns).getActions(false) : nullptr),
+    sharding_key_expr(sharding_key_ ? ExpressionAnalyzer(sharding_key_, context, nullptr, columns.columns).getActions(false) : nullptr),
     sharding_key_column_name(sharding_key_ ? sharding_key_->getColumnName() : String{}),
-    path(data_path_.empty() ? "" : (data_path_ + escapeForFileName(name) + '/'))
+    path(data_path_.empty() ? "" : (data_path_ + escapeForFileName(table_name) + '/'))
 {
 }
 
@@ -161,7 +158,7 @@ StoragePtr StorageDistributed::createWithOwnCluster(
     const Context & context_)
 {
     auto res = ext::shared_ptr_helper<StorageDistributed>::create(
-        name_, columns_, NamesAndTypesList(), NamesAndTypesList(), ColumnDefaults(),
+        name_, ColumnsDescription{columns_, NamesAndTypesList(), NamesAndTypesList(), ColumnDefaults()},
         remote_database_, remote_table_, String{}, context_, ASTPtr(), String());
 
     res->owned_cluster = owned_cluster_;
@@ -236,11 +233,9 @@ void StorageDistributed::alter(const AlterCommands & params, const String & data
             throw Exception("Storage engine " + getName() + " doesn't support primary key.", ErrorCodes::NOT_IMPLEMENTED);
 
     auto lock = lockStructureForAlter(__PRETTY_FUNCTION__);
-    params.apply(columns, materialized_columns, alias_columns, column_defaults);
+    params.apply(columns);
 
-    context.getDatabase(database_name)->alterTable(
-        context, table_name,
-        columns, materialized_columns, alias_columns, column_defaults, {});
+    context.getDatabase(database_name)->alterTable(context, table_name, columns, {});
 }
 
 
@@ -291,13 +286,13 @@ NameAndTypePair StorageDistributed::getColumn(const String & column_name) const
     if (const auto & type = VirtualColumnFactory::tryGetType(column_name))
         return { column_name, type };
 
-    return getRealColumn(column_name);
+    return columns.get(column_name);
 }
 
 
 bool StorageDistributed::hasColumn(const String & column_name) const
 {
-    return VirtualColumnFactory::hasColumn(column_name) || IStorage::hasColumn(column_name);
+    return VirtualColumnFactory::hasColumn(column_name) || columns.has(column_name);
 }
 
 void StorageDistributed::createDirectoryMonitors()
@@ -390,7 +385,7 @@ void registerStorageDistributed(StorageFactory & factory)
         /// Check that sharding_key exists in the table and has numeric type.
         if (sharding_key)
         {
-            auto sharding_expr = ExpressionAnalyzer(sharding_key, args.context, nullptr, args.columns).getActions(true);
+            auto sharding_expr = ExpressionAnalyzer(sharding_key, args.context, nullptr, args.columns.columns).getActions(true);
             const Block & block = sharding_expr->getSampleBlock();
 
             if (block.columns() != 1)
@@ -405,7 +400,6 @@ void registerStorageDistributed(StorageFactory & factory)
 
         return StorageDistributed::create(
             args.table_name, args.columns,
-            args.materialized_columns, args.alias_columns, args.column_defaults,
             remote_database, remote_table, cluster_name,
             args.context, sharding_key, args.data_path);
     });

dbms/src/Storages/System/StorageSystemColumns.cpp
@@ -18,7 +18,7 @@ namespace DB
 StorageSystemColumns::StorageSystemColumns(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.columns = NamesAndTypesList{
         { "database",           std::make_shared<DataTypeString>() },
         { "table",              std::make_shared<DataTypeString>() },
         { "name",               std::make_shared<DataTypeString>() },
@@ -137,9 +137,9 @@ BlockInputStreams StorageSystemColumns::read(
                     throw;
             }
 
-            columns = storage->getColumnsList();
-            columns.insert(std::end(columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
-            column_defaults = storage->column_defaults;
+            columns = storage->columns.getList();
+            columns.insert(std::end(columns), std::begin(storage->columns.alias), std::end(storage->columns.alias));
+            column_defaults = storage->columns.defaults;
 
             /** Info about sizes of columns for tables of MergeTree family.
               * NOTE: It is possible to add getter for this info to IStorage interface.

dbms/src/Storages/System/StorageSystemDatabases.cpp
@@ -13,7 +13,7 @@ namespace DB
 StorageSystemDatabases::StorageSystemDatabases(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.columns = NamesAndTypesList{
         {"name", std::make_shared<DataTypeString>()},
         {"engine", std::make_shared<DataTypeString>()},
         {"data_path", std::make_shared<DataTypeString>()},

dbms/src/Storages/System/StorageSystemOne.cpp
@@ -13,7 +13,7 @@ namespace DB
 StorageSystemOne::StorageSystemOne(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{{"dummy", std::make_shared<DataTypeUInt8>()}};
+    columns.columns = NamesAndTypesList{{"dummy", std::make_shared<DataTypeUInt8>()}};
 }
 
 

dbms/src/Storages/System/StorageSystemZooKeeper.cpp
@@ -22,7 +22,7 @@ namespace DB
 StorageSystemZooKeeper::StorageSystemZooKeeper(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.columns = NamesAndTypesList{
         { "name",           std::make_shared<DataTypeString>() },
         { "value",          std::make_shared<DataTypeString>() },
         { "czxid",          std::make_shared<DataTypeInt64>() },

hanzoai/datastore

dbms/src/Interpreters/ExpressionAnalyzer.cpp
@@ -190,7 +190,7 @@ ExpressionAnalyzer::ExpressionAnalyzer(
     }
 
     if (storage && source_columns.empty())
-        source_columns = storage->getSampleBlock().getNamesAndTypesList();
+        source_columns = storage->columns.getList();
     else
         removeDuplicateColumns(source_columns);
 
@@ -712,7 +712,7 @@ static std::shared_ptr<InterpreterSelectWithUnionQuery> interpretSubquery(
         /// get columns list for target table
         auto database_table = getDatabaseAndTableNameFromIdentifier(*table);
         const auto & storage = context.getTable(database_table.first, database_table.second);
-        const auto & columns = storage->getColumnsListNonMaterialized();
+        const auto & columns = storage->columns.getListNonMaterialized();
         select_expression_list->children.reserve(columns.size());
 
         /// manually substitute column names in place of asterisk
@@ -826,7 +826,9 @@ void ExpressionAnalyzer::addExternalStorage(ASTPtr & subquery_or_table_name_or_t
     Block sample = interpreter->getSampleBlock();
     NamesAndTypesList columns = sample.getNamesAndTypesList();
 
-    StoragePtr external_storage = StorageMemory::create(external_table_name, columns, NamesAndTypesList{}, NamesAndTypesList{}, ColumnDefaults{});
+    StoragePtr external_storage = StorageMemory::create(
+        external_table_name,
+        ColumnsDescription{columns, NamesAndTypesList{}, NamesAndTypesList{}, ColumnDefaults{}});
     external_storage->startup();
 
     /** We replace the subquery with the name of the temporary table.
@@ -1050,7 +1052,7 @@ void ExpressionAnalyzer::normalizeTreeImpl(
                 if (storage)
                 {
                     /// If we select from a table, get only not MATERIALIZED, not ALIAS columns.
-                    for (const auto & name_type : storage->getColumnsListNonMaterialized())
+                    for (const auto & name_type : storage->columns.getListNonMaterialized())
                         all_columns.emplace_back(std::make_shared<ASTIdentifier>(name_type.name));
                 }
                 else
@@ -1147,7 +1149,7 @@ void ExpressionAnalyzer::addAliasColumns()
     if (!storage)
         return;
 
-    source_columns.insert(std::end(source_columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
+    source_columns.insert(std::end(source_columns), std::begin(storage->columns.aliases), std::end(storage->columns.aliases));
 }
 
 

dbms/src/Storages/StorageDistributed.cpp
@@ -130,24 +130,21 @@ StorageDistributed::~StorageDistributed() = default;
 
 
 StorageDistributed::StorageDistributed(
-    const std::string & name_,
-    const NamesAndTypesList & columns_,
-    const NamesAndTypesList & materialized_columns_,
-    const NamesAndTypesList & alias_columns_,
-    const ColumnDefaults & column_defaults_,
+    const std::string & table_name_,
+    const ColumnsDescription & columns_,
     const String & remote_database_,
     const String & remote_table_,
     const String & cluster_name_,
     const Context & context_,
     const ASTPtr & sharding_key_,
     const String & data_path_)
-    : IStorage{columns_, materialized_columns_, alias_columns_, column_defaults_},
-    name(name_),
+    : IStorage{columns_},
+    table_name(table_name_),
     remote_database(remote_database_), remote_table(remote_table_),
     context(context_), cluster_name(context.getMacros().expand(cluster_name_)), has_sharding_key(sharding_key_),
-    sharding_key_expr(sharding_key_ ? ExpressionAnalyzer(sharding_key_, context, nullptr, columns).getActions(false) : nullptr),
+    sharding_key_expr(sharding_key_ ? ExpressionAnalyzer(sharding_key_, context, nullptr, columns.ordinary).getActions(false) : nullptr),
     sharding_key_column_name(sharding_key_ ? sharding_key_->getColumnName() : String{}),
-    path(data_path_.empty() ? "" : (data_path_ + escapeForFileName(name) + '/'))
+    path(data_path_.empty() ? "" : (data_path_ + escapeForFileName(table_name) + '/'))
 {
 }
 
@@ -161,7 +158,7 @@ StoragePtr StorageDistributed::createWithOwnCluster(
     const Context & context_)
 {
     auto res = ext::shared_ptr_helper<StorageDistributed>::create(
-        name_, columns_, NamesAndTypesList(), NamesAndTypesList(), ColumnDefaults(),
+        name_, ColumnsDescription{columns_, NamesAndTypesList(), NamesAndTypesList(), ColumnDefaults()},
         remote_database_, remote_table_, String{}, context_, ASTPtr(), String());
 
     res->owned_cluster = owned_cluster_;
@@ -236,11 +233,9 @@ void StorageDistributed::alter(const AlterCommands & params, const String & data
             throw Exception("Storage engine " + getName() + " doesn't support primary key.", ErrorCodes::NOT_IMPLEMENTED);
 
     auto lock = lockStructureForAlter(__PRETTY_FUNCTION__);
-    params.apply(columns, materialized_columns, alias_columns, column_defaults);
+    params.apply(columns);
 
-    context.getDatabase(database_name)->alterTable(
-        context, table_name,
-        columns, materialized_columns, alias_columns, column_defaults, {});
+    context.getDatabase(database_name)->alterTable(context, table_name, columns, {});
 }
 
 
@@ -291,13 +286,13 @@ NameAndTypePair StorageDistributed::getColumn(const String & column_name) const
     if (const auto & type = VirtualColumnFactory::tryGetType(column_name))
         return { column_name, type };
 
-    return getRealColumn(column_name);
+    return columns.get(column_name);
 }
 
 
 bool StorageDistributed::hasColumn(const String & column_name) const
 {
-    return VirtualColumnFactory::hasColumn(column_name) || IStorage::hasColumn(column_name);
+    return VirtualColumnFactory::hasColumn(column_name) || columns.has(column_name);
 }
 
 void StorageDistributed::createDirectoryMonitors()
@@ -390,7 +385,7 @@ void registerStorageDistributed(StorageFactory & factory)
         /// Check that sharding_key exists in the table and has numeric type.
         if (sharding_key)
         {
-            auto sharding_expr = ExpressionAnalyzer(sharding_key, args.context, nullptr, args.columns).getActions(true);
+            auto sharding_expr = ExpressionAnalyzer(sharding_key, args.context, nullptr, args.columns.ordinary).getActions(true);
             const Block & block = sharding_expr->getSampleBlock();
 
             if (block.columns() != 1)
@@ -405,7 +400,6 @@ void registerStorageDistributed(StorageFactory & factory)
 
         return StorageDistributed::create(
             args.table_name, args.columns,
-            args.materialized_columns, args.alias_columns, args.column_defaults,
             remote_database, remote_table, cluster_name,
             args.context, sharding_key, args.data_path);
     });

dbms/src/Storages/System/StorageSystemColumns.cpp
@@ -18,7 +18,7 @@ namespace DB
 StorageSystemColumns::StorageSystemColumns(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.ordinary = NamesAndTypesList{
         { "database",           std::make_shared<DataTypeString>() },
         { "table",              std::make_shared<DataTypeString>() },
         { "name",               std::make_shared<DataTypeString>() },
@@ -137,9 +137,9 @@ BlockInputStreams StorageSystemColumns::read(
                     throw;
             }
 
-            columns = storage->getColumnsList();
-            columns.insert(std::end(columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
-            column_defaults = storage->column_defaults;
+            columns = storage->columns.getList();
+            columns.insert(std::end(columns), std::begin(storage->columns.aliases), std::end(storage->columns.aliases));
+            column_defaults = storage->columns.defaults;
 
             /** Info about sizes of columns for tables of MergeTree family.
               * NOTE: It is possible to add getter for this info to IStorage interface.

dbms/src/Storages/System/StorageSystemDatabases.cpp
@@ -13,7 +13,7 @@ namespace DB
 StorageSystemDatabases::StorageSystemDatabases(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.ordinary = NamesAndTypesList{
         {"name", std::make_shared<DataTypeString>()},
         {"engine", std::make_shared<DataTypeString>()},
         {"data_path", std::make_shared<DataTypeString>()},

dbms/src/Storages/System/StorageSystemOne.cpp
@@ -13,7 +13,7 @@ namespace DB
 StorageSystemOne::StorageSystemOne(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{{"dummy", std::make_shared<DataTypeUInt8>()}};
+    columns.ordinary = NamesAndTypesList{{"dummy", std::make_shared<DataTypeUInt8>()}};
 }
 
 

dbms/src/Storages/System/StorageSystemZooKeeper.cpp
@@ -22,7 +22,7 @@ namespace DB
 StorageSystemZooKeeper::StorageSystemZooKeeper(const std::string & name_)
     : name(name_)
 {
-    columns = NamesAndTypesList{
+    columns.ordinary = NamesAndTypesList{
         { "name",           std::make_shared<DataTypeString>() },
         { "value",          std::make_shared<DataTypeString>() },
         { "czxid",          std::make_shared<DataTypeInt64>() },