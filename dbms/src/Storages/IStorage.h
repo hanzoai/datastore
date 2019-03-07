nohunt/ClickHouse

alter intention lock

dbms/src/Storages/IStorage.h
@@ -6,6 +6,7 @@
 #include <Core/QueryProcessingStage.h>
 #include <Databases/IDatabase.h>
 #include <Storages/ITableDeclaration.h>
+#include <Storages/TableStructureLockHolder.h>
 #include <Storages/SelectQueryInfo.h>
 #include <Interpreters/CancellationCode.h>
 #include <shared_mutex>
@@ -47,35 +48,6 @@ class MutationCommands;
 class PartitionCommands;
 
 
-/** Does not allow changing the table description (including rename and delete the table).
-  * If during any operation the table structure should remain unchanged, you need to hold such a lock for all of its time.
-  * For example, you need to hold such a lock for the duration of the entire SELECT or INSERT query and for the whole time the merge of the set of parts
-  *  (but between the selection of parts for the merge and their merging, the table structure can change).
-  * NOTE: This is a lock to "read" the table's description. To change the table description, you need to take the TableStructureWriteLock.
-  */
-class TableStructureReadLock
-{
-private:
-    friend class IStorage;
-
-    StoragePtr storage;
-    /// Order is important.
-    RWLockImpl::LockHolder data_lock;
-    RWLockImpl::LockHolder structure_lock;
-
-public:
-    TableStructureReadLock(StoragePtr storage_, bool lock_structure, bool lock_data, const String & query_id);
-};
-
-
-using TableStructureReadLockPtr = std::shared_ptr<TableStructureReadLock>;
-using TableStructureReadLocks = std::vector<TableStructureReadLockPtr>;
-
-using TableStructureWriteLock = RWLockImpl::LockHolder;
-using TableDataWriteLock = RWLockImpl::LockHolder;
-using TableFullWriteLock = std::pair<TableDataWriteLock, TableStructureWriteLock>;
-
-
 /** Storage. Responsible for
   * - storage of the table data;
   * - the definition in which files (or not in files) the data is stored;
@@ -111,50 +83,65 @@ public:
     /** Returns true if the storage supports deduplication of inserted data blocks . */
     virtual bool supportsDeduplication() const { return false; }
 
-    /** Does not allow you to change the structure or name of the table.
-      * If you change the data in the table, you will need to specify will_modify_data = true.
-      * This will take an extra lock that does not allow starting ALTER MODIFY.
-      *
-      * WARNING: You need to call methods from ITableDeclaration under such a lock. Without it, they are not thread safe.
-      * WARNING: To avoid deadlocks, this method must not be called under lock of Context.
-      */
-    TableStructureReadLockPtr lockStructure(bool will_modify_data, const String & query_id)
+
+    TableStructureLockHolder lockStructure(bool will_insert_data, const String & query_id)
     {
-        TableStructureReadLockPtr res = std::make_shared<TableStructureReadLock>(shared_from_this(), true, will_modify_data, query_id);
+        TableStructureLockHolder result;
+        if (will_insert_data)
+            result.insert_structure_lock = insert_structure_lock->getLock(RWLockImpl::Read, query_id);
+        else
+            result.select_structure_lock = select_structure_lock->getLock(RWLockImpl::Read, query_id);
+
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
+        return result;
     }
 
-    /** Does not allow reading the table structure. It is taken for ALTER, RENAME and DROP, TRUNCATE.
-      */
-    TableFullWriteLock lockForAlter(const String & query_id)
+    TableStructureLockHolder lockIntentionForAlter(const String & query_id)
     {
-        /// The calculation order is important.
-        auto res_data_lock = lockDataForAlter(query_id);
-        auto res_structure_lock = lockStructureForAlter(query_id);
-
-        return {std::move(res_data_lock), std::move(res_structure_lock)};
+        TableStructureLockHolder result;
+        result.alter_intention_lock = alter_intention_lock->getLock(RWLockImpl::Write, query_id);
+        return result;
     }
 
-    /** Does not allow changing the data in the table. (Moreover, does not give a look at the structure of the table with the intention to change the data).
-      * It is taken during write temporary data in ALTER MODIFY.
-      * Under this lock, you can take lockStructureForAlter() to change the structure of the table.
-      */
-    TableDataWriteLock lockDataForAlter(const String & query_id)
+    void lockDataForAlter(TableStructureLockHolder & lock_holder, const String & query_id)
     {
-        auto res = data_lock->getLock(RWLockImpl::Write, query_id);
+        if (!lock_holder.alter_intention_lock)
+            throw Exception("Alter intention lock for table " + getTableName() + " was not taken. This is a bug.",
+                ErrorCodes::LOGICAL_ERROR);
+
+        lock_holder.insert_structure_lock = insert_structure_lock->getLock(RWLockImpl::Write, query_id);
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
     }
 
-    TableStructureWriteLock lockStructureForAlter(const String & query_id)
+    void lockStructureForAlter(TableStructureLockHolder & lock_holder, const String & query_id)
     {
-        auto res = structure_lock->getLock(RWLockImpl::Write, query_id);
+        if (!lock_holder.alter_intention_lock)
+            throw Exception("Alter intention lock for table " + getTableName() + " was not taken. This is a bug.",
+                ErrorCodes::LOGICAL_ERROR);
+
+        if (!lock_holder.insert_structure_lock)
+            lock_holder.insert_structure_lock = insert_structure_lock->getLock(RWLockImpl::Write, query_id);
+
+        lock_holder.select_structure_lock = select_structure_lock->getLock(RWLockImpl::Write, query_id);
+
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
+    }
+
+    TableStructureLockHolder lockExclusively(const String & query_id)
+    {
+        TableStructureLockHolder result;
+
+        result.alter_intention_lock = alter_intention_lock->getLock(RWLockImpl::Write, query_id);
+        result.insert_structure_lock = insert_structure_lock->getLock(RWLockImpl::Write, query_id);
+        result.select_structure_lock = select_structure_lock->getLock(RWLockImpl::Write, query_id);
+
+        if (is_dropped)
+            throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
+
+        return result;
     }
 
     /** Returns stage to which query is going to be processed in read() function.
@@ -233,7 +220,7 @@ public:
       * This method must fully execute the ALTER query, taking care of the locks itself.
       * To update the table metadata on disk, this method should call InterpreterAlterQuery::updateMetadata.
       */
-    virtual void alter(const AlterCommands & params, const String & database_name, const String & table_name, const Context & context);
+    virtual void alter(const AlterCommands & params, const String & database_name, const String & table_name, const Context & context, TableStructureLockHolder & structure_lock);
 
     /** ALTER tables with regard to its partitions.
       * Should handle locks for each command on its own.
@@ -345,7 +332,7 @@ public:
     using std::enable_shared_from_this<IStorage>::shared_from_this;
 
 private:
-    friend class TableStructureReadLock;
+    mutable RWLock alter_intention_lock = RWLockImpl::create();
 
     /// You always need to take the next two locks in this order.
 
@@ -358,7 +345,7 @@ private:
       *  2) all changes to the data after releasing the lock will be based on the structure of the table at the time after the lock was released.
       * You need to take for read for the entire time of the operation that changes the data.
       */
-    mutable RWLock data_lock = RWLockImpl::create();
+    mutable RWLock insert_structure_lock = RWLockImpl::create();
 
     /** Lock for multiple columns and path to table. It is taken for write at RENAME, ALTER (for ALTER MODIFY for a while) and DROP.
       * It is taken for read for the whole time of SELECT, INSERT and merge parts (for MergeTree).
@@ -367,7 +354,7 @@ private:
       * That is, if this lock is taken for write, you should not worry about `parts_writing_lock`.
       * parts_writing_lock is only needed for cases when you do not want to take `table_structure_lock` for long operations (ALTER MODIFY).
       */
-    mutable RWLock structure_lock = RWLockImpl::create();
+    mutable RWLock select_structure_lock = RWLockImpl::create();
 };
 
 /// table name -> table

hanzoai/datastore

dbms/src/Storages/IStorage.h
@@ -6,6 +6,7 @@
 #include <Core/QueryProcessingStage.h>
 #include <Databases/IDatabase.h>
 #include <Storages/ITableDeclaration.h>
+#include <Storages/TableStructureLockHolder.h>
 #include <Storages/SelectQueryInfo.h>
 #include <Interpreters/CancellationCode.h>
 #include <shared_mutex>
@@ -47,35 +48,6 @@ class MutationCommands;
 class PartitionCommands;
 
 
-/** Does not allow changing the table description (including rename and delete the table).
-  * If during any operation the table structure should remain unchanged, you need to hold such a lock for all of its time.
-  * For example, you need to hold such a lock for the duration of the entire SELECT or INSERT query and for the whole time the merge of the set of parts
-  *  (but between the selection of parts for the merge and their merging, the table structure can change).
-  * NOTE: This is a lock to "read" the table's description. To change the table description, you need to take the TableStructureWriteLock.
-  */
-class TableStructureReadLock
-{
-private:
-    friend class IStorage;
-
-    StoragePtr storage;
-    /// Order is important.
-    RWLockImpl::LockHolder data_lock;
-    RWLockImpl::LockHolder structure_lock;
-
-public:
-    TableStructureReadLock(StoragePtr storage_, bool lock_structure, bool lock_data, const String & query_id);
-};
-
-
-using TableStructureReadLockPtr = std::shared_ptr<TableStructureReadLock>;
-using TableStructureReadLocks = std::vector<TableStructureReadLockPtr>;
-
-using TableStructureWriteLock = RWLockImpl::LockHolder;
-using TableDataWriteLock = RWLockImpl::LockHolder;
-using TableFullWriteLock = std::pair<TableDataWriteLock, TableStructureWriteLock>;
-
-
 /** Storage. Responsible for
   * - storage of the table data;
   * - the definition in which files (or not in files) the data is stored;
@@ -111,50 +83,66 @@ public:
     /** Returns true if the storage supports deduplication of inserted data blocks . */
     virtual bool supportsDeduplication() const { return false; }
 
-    /** Does not allow you to change the structure or name of the table.
-      * If you change the data in the table, you will need to specify will_modify_data = true.
-      * This will take an extra lock that does not allow starting ALTER MODIFY.
-      *
-      * WARNING: You need to call methods from ITableDeclaration under such a lock. Without it, they are not thread safe.
-      * WARNING: To avoid deadlocks, this method must not be called under lock of Context.
-      */
-    TableStructureReadLockPtr lockStructure(bool will_modify_data, const String & query_id)
+
+    TableStructureLockHolder lockStructure(bool will_add_new_data, const String & query_id)
     {
-        TableStructureReadLockPtr res = std::make_shared<TableStructureReadLock>(shared_from_this(), true, will_modify_data, query_id);
+        TableStructureLockHolder result;
+        if (will_add_new_data)
+            result.new_data_structure_lock = new_data_structure_lock->getLock(RWLockImpl::Read, query_id);
+        result.structure_lock = structure_lock->getLock(RWLockImpl::Read, query_id);
+
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
+        return result;
     }
 
-    /** Does not allow reading the table structure. It is taken for ALTER, RENAME and DROP, TRUNCATE.
-      */
-    TableFullWriteLock lockForAlter(const String & query_id)
+    TableStructureLockHolder lockIntentionForAlter(const String & query_id)
     {
-        /// The calculation order is important.
-        auto res_data_lock = lockDataForAlter(query_id);
-        auto res_structure_lock = lockStructureForAlter(query_id);
+        TableStructureLockHolder result;
+        result.alter_intention_lock = alter_intention_lock->getLock(RWLockImpl::Write, query_id);
 
-        return {std::move(res_data_lock), std::move(res_structure_lock)};
-    }
-
-    /** Does not allow changing the data in the table. (Moreover, does not give a look at the structure of the table with the intention to change the data).
-      * It is taken during write temporary data in ALTER MODIFY.
-      * Under this lock, you can take lockStructureForAlter() to change the structure of the table.
-      */
-    TableDataWriteLock lockDataForAlter(const String & query_id)
-    {
-        auto res = data_lock->getLock(RWLockImpl::Write, query_id);
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
+        return result;
     }
 
-    TableStructureWriteLock lockStructureForAlter(const String & query_id)
+    void lockDataForAlter(TableStructureLockHolder & lock_holder, const String & query_id)
     {
-        auto res = structure_lock->getLock(RWLockImpl::Write, query_id);
+        if (!lock_holder.alter_intention_lock)
+            throw Exception("Alter intention lock for table " + getTableName() + " was not taken. This is a bug.",
+                ErrorCodes::LOGICAL_ERROR);
+
+        lock_holder.new_data_structure_lock = new_data_structure_lock->getLock(RWLockImpl::Write, query_id);
+
         if (is_dropped)
             throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
-        return res;
+    }
+
+    void lockStructureForAlter(TableStructureLockHolder & lock_holder, const String & query_id)
+    {
+        if (!lock_holder.alter_intention_lock)
+            throw Exception("Alter intention lock for table " + getTableName() + " was not taken. This is a bug.",
+                ErrorCodes::LOGICAL_ERROR);
+
+        if (!lock_holder.new_data_structure_lock)
+            lock_holder.new_data_structure_lock = new_data_structure_lock->getLock(RWLockImpl::Write, query_id);
+        lock_holder.structure_lock = structure_lock->getLock(RWLockImpl::Write, query_id);
+
+        if (is_dropped)
+            throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
+    }
+
+    TableStructureLockHolder lockExclusively(const String & query_id)
+    {
+        TableStructureLockHolder result;
+        result.alter_intention_lock = alter_intention_lock->getLock(RWLockImpl::Write, query_id);
+        result.new_data_structure_lock = new_data_structure_lock->getLock(RWLockImpl::Write, query_id);
+        result.structure_lock = structure_lock->getLock(RWLockImpl::Write, query_id);
+
+        if (is_dropped)
+            throw Exception("Table is dropped", ErrorCodes::TABLE_IS_DROPPED);
+
+        return result;
     }
 
     /** Returns stage to which query is going to be processed in read() function.
@@ -233,7 +221,7 @@ public:
       * This method must fully execute the ALTER query, taking care of the locks itself.
       * To update the table metadata on disk, this method should call InterpreterAlterQuery::updateMetadata.
       */
-    virtual void alter(const AlterCommands & params, const String & database_name, const String & table_name, const Context & context);
+    virtual void alter(const AlterCommands & params, const String & database_name, const String & table_name, const Context & context, TableStructureLockHolder & structure_lock);
 
     /** ALTER tables with regard to its partitions.
       * Should handle locks for each command on its own.
@@ -345,9 +333,9 @@ public:
     using std::enable_shared_from_this<IStorage>::shared_from_this;
 
 private:
-    friend class TableStructureReadLock;
+    /// You always need to take the next three locks in this order.
 
-    /// You always need to take the next two locks in this order.
+    mutable RWLock alter_intention_lock = RWLockImpl::create();
 
     /** It is taken for read for the entire INSERT query and the entire merge of the parts (for MergeTree).
       * It is taken for write for the entire time ALTER MODIFY.
@@ -358,7 +346,7 @@ private:
       *  2) all changes to the data after releasing the lock will be based on the structure of the table at the time after the lock was released.
       * You need to take for read for the entire time of the operation that changes the data.
       */
-    mutable RWLock data_lock = RWLockImpl::create();
+    mutable RWLock new_data_structure_lock = RWLockImpl::create();
 
     /** Lock for multiple columns and path to table. It is taken for write at RENAME, ALTER (for ALTER MODIFY for a while) and DROP.
       * It is taken for read for the whole time of SELECT, INSERT and merge parts (for MergeTree).