filimonov/ClickHouse

SensitiveDataMasker

dbms/src/Common/ProfileEvents.cpp
@@ -44,6 +44,8 @@
     M(NetworkSendElapsedMicroseconds, "") \
     M(ThrottlerSleepMicroseconds, "Total time a query was sleeping to conform the 'max_network_bandwidth' setting.") \
     \
+    M(QueryMaskingRulesMatches, "Number of times query masking rules was successfully matched.") \
+    \
     M(ReplicatedPartFetches, "Number of times a data part was downloaded from replica of a ReplicatedMergeTree table.") \
     M(ReplicatedPartFailedFetches, "") \
     M(ObsoleteReplicatedParts, "") \

hanzoai/datastore

dbms/src/Common/ProfileEvents.cpp
@@ -44,6 +44,8 @@
     M(NetworkSendElapsedMicroseconds, "") \
     M(ThrottlerSleepMicroseconds, "Total time a query was sleeping to conform the 'max_network_bandwidth' setting.") \
     \
+    M(QueryMaskingRulesMatch, "Number of times query masking rules was successfully matched.") \
+    \
     M(ReplicatedPartFetches, "Number of times a data part was downloaded from replica of a ReplicatedMergeTree table.") \
     M(ReplicatedPartFailedFetches, "") \
     M(ObsoleteReplicatedParts, "") \