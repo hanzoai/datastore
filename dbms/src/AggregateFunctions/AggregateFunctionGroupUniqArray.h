shunpeizhang/ClickHouse

Key memory management for compound hash tables.
Some aggregation methods initially emplace a temporary StringRef key
into a hash table. Then, if the key was not seen before, they make a
persistent copy of the key and update the hash table with it. This
approach is not suitable for compound hash tables, because the logic of
when the persistent key is needed is more complex, and is contained
within the hash table itself.

In this commit, we switch to managing key memory with callbacks passed
to the hash table, that allow it to request a persistent copy of the key
if it is needed. This should be more appropriate for compound hash
tables.

This commit prepares for StringHashMap PR #5417.

dbms/src/AggregateFunctions/AggregateFunctionGroupUniqArray.h
@@ -10,6 +10,7 @@
 #include <Columns/ColumnArray.h>
 
 #include <Common/HashTable/HashSet.h>
+#include <Common/HashTable/HashTableKeyHolder.h>
 
 #include <AggregateFunctions/IAggregateFunction.h>
 
@@ -131,11 +132,6 @@ struct AggregateFunctionGroupUniqArrayGenericData
     Set value;
 };
 
-
-/// Helper function for deserialize and insert for the class AggregateFunctionGroupUniqArrayGeneric
-template <bool is_plain_column>
-static StringRef getSerializationImpl(const IColumn & column, size_t row_num, Arena & arena);
-
 template <bool is_plain_column>
 static void deserializeAndInsertImpl(StringRef str, IColumn & data_to);
 
@@ -153,9 +149,21 @@ class AggregateFunctionGroupUniqArrayGeneric
 
     using State = AggregateFunctionGroupUniqArrayGenericData;
 
-    static StringRef getSerialization(const IColumn & column, size_t row_num, Arena & arena)
+    using KeyHolder = typename std::conditional<is_plain_column,
+        ArenaKeyHolder, SerializedKeyHolder>::type;
+
+    static KeyHolder getKeyHolder(const IColumn & column, size_t row_num, Arena & arena)
     {
-        return getSerializationImpl<is_plain_column>(column, row_num, arena);
+        if constexpr (is_plain_column)
+        {
+            return ArenaKeyHolder{column.getDataAt(row_num), arena};
+        }
+        else
+        {
+            const char * begin = nullptr;
+            StringRef serialized = column.serializeValueIntoArena(row_num, arena, begin);
+            return SerializedKeyHolder{serialized, arena};
+        }
     }
 
     static void deserializeAndInsert(StringRef str, IColumn & data_to)
@@ -208,26 +216,13 @@ public:
     void add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena * arena) const override
     {
         auto & set = this->data(place).value;
+        if (limit_num_elems && set.size() >= max_elems)
+            return;
 
         bool inserted;
         State::Set::iterator it;
-
-        if (limit_num_elems && set.size() >= max_elems)
-            return;
-        StringRef str_serialized = getSerialization(*columns[0], row_num, *arena);
-
-        set.emplace(str_serialized, it, inserted);
-
-        if constexpr (!is_plain_column)
-        {
-            if (!inserted)
-                arena->rollback(str_serialized.size);
-        }
-        else
-        {
-            if (inserted)
-                it->getValueMutable().data = arena->insert(str_serialized.data, str_serialized.size);
-        }
+        auto key_holder = getKeyHolder(*columns[0], row_num, *arena);
+        set.emplaceKeyHolder(key_holder, it, inserted);
     }
 
     void merge(AggregateDataPtr place, ConstAggregateDataPtr rhs, Arena * arena) const override
@@ -240,15 +235,12 @@ public:
         for (auto & rhs_elem : rhs_set)
         {
             if (limit_num_elems && cur_set.size() >= max_elems)
-                return ;
-            cur_set.emplace(rhs_elem.getValue(), it, inserted);
-            if (inserted)
-            {
-                if (it->getValue().size)
-                    it->getValueMutable().data = arena->insert(it->getValue().data, it->getValue().size);
-                else
-                    it->getValueMutable().data = nullptr;
-            }
+                return;
+
+            // We have to copy the keys to our arena.
+            assert(arena != nullptr);
+            cur_set.emplaceKeyHolder(ArenaKeyHolder{rhs_elem.getValue(), *arena},
+                               it, inserted);
         }
     }
 
@@ -269,20 +261,6 @@ public:
 
     const char * getHeaderFilePath() const override { return __FILE__; }
 };
-
-
-template <>
-inline StringRef getSerializationImpl<false>(const IColumn & column, size_t row_num, Arena & arena)
-{
-    const char * begin = nullptr;
-    return column.serializeValueIntoArena(row_num, arena, begin);
-}
-
-template <>
-inline StringRef getSerializationImpl<true>(const IColumn & column, size_t row_num, Arena &)
-{
-    return column.getDataAt(row_num);
-}
 
 template <>
 inline void deserializeAndInsertImpl<false>(StringRef str, IColumn & data_to)

dbms/src/Common/ColumnsHashingImpl.h
@@ -115,26 +115,22 @@ public:
     template <typename Data>
     ALWAYS_INLINE EmplaceResult emplaceKey(Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        return emplaceKeyImpl(key, data, pool);
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return emplaceKeyHolderImpl(key_holder, data);
     }
 
     template <typename Data>
     ALWAYS_INLINE FindResult findKey(Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        auto res = findKeyImpl(key, data);
-        static_cast<Derived &>(*this).onExistingKey(key, pool);
-        return res;
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return findKeyImpl(*key_holder, data);
     }
 
     template <typename Data>
     ALWAYS_INLINE size_t getHash(const Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        auto res = data.hash(key);
-        static_cast<Derived &>(*this).onExistingKey(key, pool);
-        return res;
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return data.hash(*key_holder);
     }
 
 protected:
@@ -155,20 +151,13 @@ protected:
         }
     }
 
-    template <typename Key>
-    static ALWAYS_INLINE void onNewKey(Key & /*key*/, Arena & /*pool*/) {}
-    template <typename Key>
-    static ALWAYS_INLINE void onExistingKey(Key & /*key*/, Arena & /*pool*/) {}
-
-    template <typename Data, typename Key>
-    ALWAYS_INLINE EmplaceResult emplaceKeyImpl(Key key, Data & data, Arena & pool)
+    template <typename Data, typename KeyHolder>
+    ALWAYS_INLINE EmplaceResult emplaceKeyHolderImpl(KeyHolder & key_holder, Data & data)
     {
         if constexpr (Cache::consecutive_keys_optimization)
         {
-            if (cache.found && cache.check(key))
+            if (cache.found && cache.check(*key_holder))
             {
-                static_cast<Derived &>(*this).onExistingKey(key, pool);
-
                 if constexpr (has_mapped)
                     return EmplaceResult(cache.value.second, cache.value.second, false);
                 else
@@ -178,7 +167,7 @@ protected:
 
         typename Data::iterator it;
         bool inserted = false;
-        data.emplace(key, it, inserted);
+        data.emplaceKeyHolder(key_holder, it, inserted);
 
         [[maybe_unused]] Mapped * cached = nullptr;
         if constexpr (has_mapped)
@@ -189,13 +178,8 @@ protected:
             if constexpr (has_mapped)
             {
                 new(&it->getSecond()) Mapped();
-                static_cast<Derived &>(*this).onNewKey(it->getFirstMutable(), pool);
             }
-            else
-                static_cast<Derived &>(*this).onNewKey(it->getValueMutable(), pool);
         }
-        else
-            static_cast<Derived &>(*this).onExistingKey(key, pool);
 
         if constexpr (consecutive_keys_optimization)
         {

dbms/src/Common/HashTable/TwoLevelHashTable.h
@@ -235,22 +235,34 @@ public:
       * if (inserted)
       *     new(&it->second) Mapped(value);
       */
-    void ALWAYS_INLINE emplace(Key x, iterator & it, bool & inserted)
+    template <typename KeyHolder>
+    void ALWAYS_INLINE emplaceKeyHolder(KeyHolder && key_holder, iterator & it, bool & inserted)
     {
-        size_t hash_value = hash(x);
-        emplace(x, it, inserted, hash_value);
+        size_t hash_value = hash(*key_holder);
+        emplaceKeyHolder(key_holder, it, inserted, hash_value);
+    }
+
+    void ALWAYS_INLINE emplace(Key key, iterator & it, bool & inserted)
+    {
+        emplaceKeyHolder(NoopKeyHolder(key), it, inserted);
     }
 
 
     /// Same, but with a precalculated values of hash function.
-    void ALWAYS_INLINE emplace(Key x, iterator & it, bool & inserted, size_t hash_value)
+    template <typename KeyHolder>
+    void ALWAYS_INLINE emplaceKeyHolder(KeyHolder && key_holder, iterator & it,
+                                  bool & inserted, size_t hash_value)
     {
         size_t buck = getBucketFromHash(hash_value);
         typename Impl::iterator impl_it;
-        impls[buck].emplace(x, impl_it, inserted, hash_value);
+        impls[buck].emplaceKeyHolder(key_holder, impl_it, inserted, hash_value);
         it = iterator(this, buck, impl_it);
     }
 
+    void ALWAYS_INLINE emplace(Key key, iterator & it, bool & inserted, size_t hash_value)
+    {
+        emplaceKeyHolder(NoopKeyHolder(key), it, inserted, hash_value);
+    }
 
     iterator ALWAYS_INLINE find(Key x, size_t hash_value)
     {

hanzoai/datastore

dbms/src/AggregateFunctions/AggregateFunctionGroupUniqArray.h
@@ -10,6 +10,7 @@
 #include <Columns/ColumnArray.h>
 
 #include <Common/HashTable/HashSet.h>
+#include <Common/HashTable/HashTableKeyHolder.h>
 #include <Common/assert_cast.h>
 
 #include <AggregateFunctions/IAggregateFunction.h>
@@ -132,11 +133,6 @@ struct AggregateFunctionGroupUniqArrayGenericData
     Set value;
 };
 
-
-/// Helper function for deserialize and insert for the class AggregateFunctionGroupUniqArrayGeneric
-template <bool is_plain_column>
-static StringRef getSerializationImpl(const IColumn & column, size_t row_num, Arena & arena);
-
 template <bool is_plain_column>
 static void deserializeAndInsertImpl(StringRef str, IColumn & data_to);
 
@@ -154,9 +150,18 @@ class AggregateFunctionGroupUniqArrayGeneric
 
     using State = AggregateFunctionGroupUniqArrayGenericData;
 
-    static StringRef getSerialization(const IColumn & column, size_t row_num, Arena & arena)
+    static auto getKeyHolder(const IColumn & column, size_t row_num, Arena & arena)
     {
-        return getSerializationImpl<is_plain_column>(column, row_num, arena);
+        if constexpr (is_plain_column)
+        {
+            return ArenaKeyHolder{column.getDataAt(row_num), arena};
+        }
+        else
+        {
+            const char * begin = nullptr;
+            StringRef serialized = column.serializeValueIntoArena(row_num, arena, begin);
+            return SerializedKeyHolder{serialized, arena};
+        }
     }
 
     static void deserializeAndInsert(StringRef str, IColumn & data_to)
@@ -209,26 +214,13 @@ public:
     void add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena * arena) const override
     {
         auto & set = this->data(place).value;
+        if (limit_num_elems && set.size() >= max_elems)
+            return;
 
         bool inserted;
         State::Set::iterator it;
-
-        if (limit_num_elems && set.size() >= max_elems)
-            return;
-        StringRef str_serialized = getSerialization(*columns[0], row_num, *arena);
-
-        set.emplace(str_serialized, it, inserted);
-
-        if constexpr (!is_plain_column)
-        {
-            if (!inserted)
-                arena->rollback(str_serialized.size);
-        }
-        else
-        {
-            if (inserted)
-                it->getValueMutable().data = arena->insert(str_serialized.data, str_serialized.size);
-        }
+        auto key_holder = getKeyHolder(*columns[0], row_num, *arena);
+        set.emplace(key_holder, it, inserted);
     }
 
     void merge(AggregateDataPtr place, ConstAggregateDataPtr rhs, Arena * arena) const override
@@ -241,15 +233,11 @@ public:
         for (auto & rhs_elem : rhs_set)
         {
             if (limit_num_elems && cur_set.size() >= max_elems)
-                return ;
-            cur_set.emplace(rhs_elem.getValue(), it, inserted);
-            if (inserted)
-            {
-                if (it->getValue().size)
-                    it->getValueMutable().data = arena->insert(it->getValue().data, it->getValue().size);
-                else
-                    it->getValueMutable().data = nullptr;
-            }
+                return;
+
+            // We have to copy the keys to our arena.
+            assert(arena != nullptr);
+            cur_set.emplace(ArenaKeyHolder{rhs_elem.getValue(), *arena}, it, inserted);
         }
     }
 
@@ -270,20 +258,6 @@ public:
 
     const char * getHeaderFilePath() const override { return __FILE__; }
 };
-
-
-template <>
-inline StringRef getSerializationImpl<false>(const IColumn & column, size_t row_num, Arena & arena)
-{
-    const char * begin = nullptr;
-    return column.serializeValueIntoArena(row_num, arena, begin);
-}
-
-template <>
-inline StringRef getSerializationImpl<true>(const IColumn & column, size_t row_num, Arena &)
-{
-    return column.getDataAt(row_num);
-}
 
 template <>
 inline void deserializeAndInsertImpl<false>(StringRef str, IColumn & data_to)

dbms/src/Common/ColumnsHashingImpl.h
@@ -2,6 +2,7 @@
 
 #include <Columns/IColumn.h>
 #include <Common/assert_cast.h>
+#include <Common/HashTable/HashTableKeyHolder.h>
 #include <Interpreters/AggregationCommon.h>
 
 
@@ -117,26 +118,22 @@ public:
     template <typename Data>
     ALWAYS_INLINE EmplaceResult emplaceKey(Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        return emplaceKeyImpl(key, data, pool);
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return emplaceImpl(key_holder, data);
     }
 
     template <typename Data>
     ALWAYS_INLINE FindResult findKey(Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        auto res = findKeyImpl(key, data);
-        static_cast<Derived &>(*this).onExistingKey(key, pool);
-        return res;
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return findKeyImpl(keyHolderGetKey(key_holder), data);
     }
 
     template <typename Data>
     ALWAYS_INLINE size_t getHash(const Data & data, size_t row, Arena & pool)
     {
-        auto key = static_cast<Derived &>(*this).getKey(row, pool);
-        auto res = data.hash(key);
-        static_cast<Derived &>(*this).onExistingKey(key, pool);
-        return res;
+        auto key_holder = static_cast<Derived &>(*this).getKeyHolder(row, pool);
+        return data.hash(keyHolderGetKey(key_holder));
     }
 
 protected:
@@ -157,20 +154,13 @@ protected:
         }
     }
 
-    template <typename Key>
-    static ALWAYS_INLINE void onNewKey(Key & /*key*/, Arena & /*pool*/) {}
-    template <typename Key>
-    static ALWAYS_INLINE void onExistingKey(Key & /*key*/, Arena & /*pool*/) {}
-
-    template <typename Data, typename Key>
-    ALWAYS_INLINE EmplaceResult emplaceKeyImpl(Key key, Data & data, Arena & pool)
+    template <typename Data, typename KeyHolder>
+    ALWAYS_INLINE EmplaceResult emplaceImpl(KeyHolder & key_holder, Data & data)
     {
         if constexpr (Cache::consecutive_keys_optimization)
         {
-            if (cache.found && cache.check(key))
+            if (cache.found && cache.check(keyHolderGetKey(key_holder)))
             {
-                static_cast<Derived &>(*this).onExistingKey(key, pool);
-
                 if constexpr (has_mapped)
                     return EmplaceResult(cache.value.second, cache.value.second, false);
                 else
@@ -180,7 +170,7 @@ protected:
 
         typename Data::iterator it;
         bool inserted = false;
-        data.emplace(key, it, inserted);
+        data.emplace(key_holder, it, inserted);
 
         [[maybe_unused]] Mapped * cached = nullptr;
         if constexpr (has_mapped)
@@ -191,13 +181,8 @@ protected:
             if constexpr (has_mapped)
             {
                 new(&it->getSecond()) Mapped();
-                static_cast<Derived &>(*this).onNewKey(it->getFirstMutable(), pool);
             }
-            else
-                static_cast<Derived &>(*this).onNewKey(it->getValueMutable(), pool);
         }
-        else
-            static_cast<Derived &>(*this).onExistingKey(key, pool);
 
         if constexpr (consecutive_keys_optimization)
         {

dbms/src/Common/HashTable/TwoLevelHashTable.h
@@ -235,19 +235,22 @@ public:
       * if (inserted)
       *     new(&it->second) Mapped(value);
       */
-    void ALWAYS_INLINE emplace(Key x, iterator & it, bool & inserted)
+    template <typename KeyHolder>
+    void ALWAYS_INLINE emplace(KeyHolder && key_holder, iterator & it, bool & inserted)
     {
-        size_t hash_value = hash(x);
-        emplace(x, it, inserted, hash_value);
+        size_t hash_value = hash(keyHolderGetKey(key_holder));
+        emplace(key_holder, it, inserted, hash_value);
     }
 
 
     /// Same, but with a precalculated values of hash function.
-    void ALWAYS_INLINE emplace(Key x, iterator & it, bool & inserted, size_t hash_value)
+    template <typename KeyHolder>
+    void ALWAYS_INLINE emplace(KeyHolder && key_holder, iterator & it,
+                                  bool & inserted, size_t hash_value)
     {
         size_t buck = getBucketFromHash(hash_value);
         typename Impl::iterator impl_it;
-        impls[buck].emplace(x, impl_it, inserted, hash_value);
+        impls[buck].emplace(key_holder, impl_it, inserted, hash_value);
         it = iterator(this, buck, impl_it);
     }
 