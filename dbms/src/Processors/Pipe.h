vitlibar/ClickHouse

Rework Quota based on IAccessEntity.

dbms/src/Processors/Pipe.h
@@ -8,8 +8,6 @@ namespace DB
 class Pipe;
 using Pipes = std::vector<Pipe>;
 
-class QuotaForIntervals;
-
 /// Pipe is a set of processors which represents the part of pipeline with single output.
 /// All processors in pipe are connected. All ports are connected except the output one.
 class Pipe
@@ -39,7 +37,7 @@ public:
 
     /// Specify quotas and limits for every ISourceWithProgress.
     void setLimits(const SourceWithProgress::LocalLimits & limits);
-    void setQuota(QuotaForIntervals & quota);
+    void setQuota(const std::shared_ptr<QuotaUsageContext> & quota);
 
     /// Set information about preferred executor number for sources.
     void pinSources(size_t executor_number);

dbms/src/Processors/Sources/SourceWithProgress.cpp
@@ -1,7 +1,7 @@
 #include <Processors/Sources/SourceWithProgress.h>
 
 #include <Interpreters/ProcessList.h>
-#include <Interpreters/Quota.h>
+#include <Access/QuotaUsageContext.h>
 
 namespace DB
 {
@@ -72,10 +72,8 @@ void SourceWithProgress::progress(const Progress & value)
         /// It is here for compatibility with IBlockInputsStream.
         limits.speed_limits.throttle(progress.read_rows, progress.read_bytes, total_rows, total_elapsed_microseconds);
 
-        if (quota != nullptr && limits.mode == LimitsMode::LIMITS_TOTAL)
-        {
-            quota->checkAndAddReadRowsBytes(time(nullptr), value.read_rows, value.read_bytes);
-        }
+        if (quota && limits.mode == LimitsMode::LIMITS_TOTAL)
+            quota->used({Quota::READ_ROWS, value.read_rows}, {Quota::READ_BYTES, value.read_bytes});
     }
 }
 

hanzoai/datastore

dbms/src/Processors/Pipe.h
@@ -8,8 +8,6 @@ namespace DB
 class Pipe;
 using Pipes = std::vector<Pipe>;
 
-class QuotaForIntervals;
-
 /// Pipe is a set of processors which represents the part of pipeline with single output.
 /// All processors in pipe are connected. All ports are connected except the output one.
 class Pipe
@@ -39,7 +37,7 @@ public:
 
     /// Specify quotas and limits for every ISourceWithProgress.
     void setLimits(const SourceWithProgress::LocalLimits & limits);
-    void setQuota(QuotaForIntervals & quota);
+    void setQuota(const std::shared_ptr<QuotaContext> & quota);
 
     /// Set information about preferred executor number for sources.
     void pinSources(size_t executor_number);

dbms/src/Processors/Sources/SourceWithProgress.cpp
@@ -1,7 +1,7 @@
 #include <Processors/Sources/SourceWithProgress.h>
 
 #include <Interpreters/ProcessList.h>
-#include <Interpreters/Quota.h>
+#include <Access/QuotaContext.h>
 
 namespace DB
 {
@@ -72,10 +72,8 @@ void SourceWithProgress::progress(const Progress & value)
         /// It is here for compatibility with IBlockInputsStream.
         limits.speed_limits.throttle(progress.read_rows, progress.read_bytes, total_rows, total_elapsed_microseconds);
 
-        if (quota != nullptr && limits.mode == LimitsMode::LIMITS_TOTAL)
-        {
-            quota->checkAndAddReadRowsBytes(time(nullptr), value.read_rows, value.read_bytes);
-        }
+        if (quota && limits.mode == LimitsMode::LIMITS_TOTAL)
+            quota->used({Quota::READ_ROWS, value.read_rows}, {Quota::READ_BYTES, value.read_bytes});
     }
 }
 