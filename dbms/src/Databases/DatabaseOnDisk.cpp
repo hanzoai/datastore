zlareb1/ClickHouse

make data path relative

dbms/src/Databases/DatabaseOnDisk.cpp
@@ -220,9 +220,9 @@ ASTPtr DatabaseOnDisk::getCreateDatabaseQuery(const IDatabase & database, const 
     return ast;
 }
 
-void DatabaseOnDisk::drop(const IDatabase & database)
+void DatabaseOnDisk::drop(const IDatabase & database, const Context & context)
 {
-    Poco::File(database.getDataPath()).remove(false);
+    Poco::File(context.getPath() + database.getDataPath()).remove(false);
     Poco::File(database.getMetadataPath()).remove(false);
 }
 
@@ -248,7 +248,7 @@ time_t DatabaseOnDisk::getTableMetadataModificationTime(
     }
 }
 
-void DatabaseOnDisk::iterateTableFiles(const IDatabase & database, Poco::Logger * log, const IteratingFunction & iterating_function)
+void DatabaseOnDisk::iterateTableFiles(const IDatabase & database, Poco::Logger * log, const Context & context, const IteratingFunction & iterating_function)
 {
     Poco::DirectoryIterator dir_end;
     for (Poco::DirectoryIterator dir_it(database.getMetadataPath()); dir_it != dir_end; ++dir_it)
@@ -266,7 +266,7 @@ void DatabaseOnDisk::iterateTableFiles(const IDatabase & database, Poco::Logger 
         if (endsWith(dir_it.name(), tmp_drop_ext))
         {
             const std::string table_name = dir_it.name().substr(0, dir_it.name().size() - strlen(tmp_drop_ext));
-            if (Poco::File(database.getDataPath() + '/' + table_name).exists())
+            if (Poco::File(context.getPath() + database.getDataPath() + '/' + table_name).exists())
             {
                 Poco::File(dir_it->path()).renameTo(table_name + ".sql");
                 LOG_WARNING(log, "Table " << backQuote(table_name) << " was not dropped previously");

dbms/src/Databases/DatabaseOnDisk.h
@@ -60,7 +60,7 @@ public:
         const IDatabase & database,
         const Context & context);
 
-    static void drop(const IDatabase & database);
+    static void drop(const IDatabase & database, const Context & context);
 
     static String getTableMetadataPath(
         const IDatabase & database,
@@ -72,7 +72,7 @@ public:
 
 
     using IteratingFunction = std::function<void(const String &)>;
-    static void iterateTableFiles(const IDatabase & database, Poco::Logger * log, const IteratingFunction & iterating_function);
+    static void iterateTableFiles(const IDatabase & database, Poco::Logger * log, const Context & context, const IteratingFunction & iterating_function);
 
 private:
     static ASTPtr getCreateTableQueryImpl(

hanzoai/datastore

dbms/src/Databases/DatabaseOnDisk.cpp
@@ -493,9 +493,9 @@ ASTPtr DatabaseOnDisk::getCreateDatabaseQuery(const IDatabase & database, const 
     return ast;
 }
 
-void DatabaseOnDisk::drop(const IDatabase & database)
+void DatabaseOnDisk::drop(const IDatabase & database, const Context & context)
 {
-    Poco::File(database.getDataPath()).remove(false);
+    Poco::File(context.getPath() + database.getDataPath()).remove(false);
     Poco::File(database.getMetadataPath()).remove(false);
 }
 
@@ -517,7 +517,7 @@ time_t DatabaseOnDisk::getObjectMetadataModificationTime(
         return static_cast<time_t>(0);
 }
 
-void DatabaseOnDisk::iterateMetadataFiles(const IDatabase & database, Poco::Logger * log, const IteratingFunction & iterating_function)
+void DatabaseOnDisk::iterateMetadataFiles(const IDatabase & database, Poco::Logger * log, const Context & context, const IteratingFunction & iterating_function)
 {
     Poco::DirectoryIterator dir_end;
     for (Poco::DirectoryIterator dir_it(database.getMetadataPath()); dir_it != dir_end; ++dir_it)
@@ -535,7 +535,7 @@ void DatabaseOnDisk::iterateMetadataFiles(const IDatabase & database, Poco::Logg
         if (endsWith(dir_it.name(), tmp_drop_ext))
         {
             const std::string object_name = dir_it.name().substr(0, dir_it.name().size() - strlen(tmp_drop_ext));
-            if (Poco::File(database.getDataPath() + '/' + object_name).exists())
+            if (Poco::File(context.getPath() + database.getDataPath() + '/' + object_name).exists())
             {
                 /// TODO maybe complete table drop and remove all table data (including data on other volumes and metadata in ZK)
                 Poco::File(dir_it->path()).renameTo(database.getMetadataPath() + object_name + ".sql");

dbms/src/Databases/DatabaseOnDisk.h
@@ -98,7 +98,7 @@ public:
         const IDatabase & database,
         const Context & context);
 
-    static void drop(const IDatabase & database);
+    static void drop(const IDatabase & database, const Context & context);
 
     static String getObjectMetadataPath(
         const IDatabase & database,
@@ -110,7 +110,7 @@ public:
 
 
     using IteratingFunction = std::function<void(const String &)>;
-    static void iterateMetadataFiles(const IDatabase & database, Poco::Logger * log, const IteratingFunction & iterating_function);
+    static void iterateMetadataFiles(const IDatabase & database, Poco::Logger * log, const Context & context, const IteratingFunction & iterating_function);
 
 private:
     static ASTPtr getCreateTableQueryImpl(