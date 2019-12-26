ClickHouse/ClickHouse

better constructors of storages

dbms/src/Databases/DatabaseMySQL.cpp
@@ -235,7 +235,7 @@ DatabaseMySQL::MySQLStorageInfo DatabaseMySQL::createStorageInfo(
     const String & table_name, const NamesAndTypesList & columns_name_and_type, const UInt64 & table_modification_time) const
 {
     const auto & mysql_table = StorageMySQL::create(
-            database_name, table_name, std::move(mysql_pool), mysql_database_name, table_name,
+            StorageID(database_name, table_name), std::move(mysql_pool), mysql_database_name, table_name,
             false, "", ColumnsDescription{columns_name_and_type}, ConstraintsDescription{}, global_context);
 
     const auto & create_table_query = std::make_shared<ASTCreateQuery>();

hanzoai/datastore

dbms/src/Databases/DatabaseMySQL.cpp
@@ -239,7 +239,7 @@ void DatabaseMySQL::fetchLatestTablesStructureIntoCache(const std::map<String, U
         }
 
         local_tables_cache[table_name] = std::make_pair(table_modification_time, StorageMySQL::create(
-            database_name, table_name, std::move(mysql_pool), database_name_in_mysql, table_name,
+            StorageID(database_name, table_name), std::move(mysql_pool), database_name_in_mysql, table_name,
             false, "", ColumnsDescription{columns_name_and_type}, ConstraintsDescription{}, global_context));
     }
 }