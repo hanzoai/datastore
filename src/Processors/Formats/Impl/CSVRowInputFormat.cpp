panda-sheep/ClickHouse

better diagnostic info in input formats

src/Processors/Formats/Impl/CSVRowInputFormat.cpp
@@ -273,6 +273,7 @@ bool CSVRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns & columns,
             return false;
         }
 
+        skipWhitespacesAndTabs(in);
         if (column_indexes_for_input_fields[file_column].has_value())
         {
             auto & header = getPort().getHeader();
@@ -289,6 +290,7 @@ bool CSVRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns & columns,
             if (!deserializeFieldAndPrintDiagnosticInfo(skipped_column_str, skipped_column_type, *skipped_column, out, file_column))
                 return false;
         }
+        skipWhitespacesAndTabs(in);
 
         /// Delimiters
         if (file_column + 1 == column_indexes_for_input_fields.size())
@@ -351,12 +353,8 @@ void CSVRowInputFormat::syncAfterError()
     skipToNextLineOrEOF(in);
 }
 
-void CSVRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                            ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos)
+void CSVRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column)
 {
-    skipWhitespacesAndTabs(in);
-    prev_pos = in.position();
-
     if (column_indexes_for_input_fields[file_column])
     {
         const bool is_last_file_column = file_column + 1 == column_indexes_for_input_fields.size();
@@ -367,9 +365,6 @@ void CSVRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & 
         String tmp;
         readCSVString(tmp, in, format_settings.csv);
     }
-
-    curr_pos = in.position();
-    skipWhitespacesAndTabs(in);
 }
 
 bool CSVRowInputFormat::readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column)

src/Processors/Formats/Impl/CSVRowInputFormat.h
@@ -55,11 +55,10 @@ private:
     void addInputColumn(const String & column_name);
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                             ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t, ReadBuffer::Position pos) override
     {
-        return *pos != '\n' && *pos != '\r' && *pos != format_settings.csv.delimiter;
+        return *pos != '\n' && *pos != '\r' && *pos != format_settings.csv.delimiter && *pos != ' ' && *pos != '\t';
     }
 
     bool readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column);

src/Processors/Formats/Impl/TabSeparatedRowInputFormat.cpp
@@ -318,10 +318,8 @@ bool TabSeparatedRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns &
     return true;
 }
 
-void TabSeparatedRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                                     ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos)
+void TabSeparatedRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column)
 {
-    prev_pos = in.position();
     if (column_indexes_for_input_fields[file_column])
     {
         const bool is_last_file_column = file_column + 1 == column_indexes_for_input_fields.size();
@@ -332,7 +330,6 @@ void TabSeparatedRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, I
         NullSink null_sink;
         readEscapedStringInto(null_sink, in);
     }
-    curr_pos = in.position();
 }
 
 void TabSeparatedRowInputFormat::syncAfterError()

src/Processors/Formats/Impl/TabSeparatedRowInputFormat.h
@@ -50,8 +50,7 @@ private:
     void fillUnreadColumnsWithDefaults(MutableColumns & columns, RowReadExtension & row_read_extension);
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                             ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t, ReadBuffer::Position pos) override { return *pos != '\n' && *pos != '\t'; }
 };
 

src/Processors/Formats/Impl/TemplateRowInputFormat.h
@@ -42,8 +42,7 @@ private:
     [[noreturn]] void throwUnexpectedEof();
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column, ReadBuffer::Position & prev_pos,
-                             ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t after_col_idx, ReadBuffer::Position pos) override;
     void writeErrorStringForWrongDelimiter(WriteBuffer & out, const String & description, const String & delim);
 

src/Processors/Formats/RowInputFormatWithDiagnosticInfo.h
@@ -24,8 +24,7 @@ protected:
                                                 WriteBuffer & out, size_t file_column);
 
     virtual bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) = 0;
-    virtual void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                     ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) = 0;
+    virtual void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column) = 0;
     virtual bool isGarbageAfterField(size_t after_input_pos_idx, ReadBuffer::Position pos) = 0;
 
     /// For convenient diagnostics in case of an error.

hanzoai/datastore

src/Processors/Formats/Impl/CSVRowInputFormat.cpp
@@ -273,6 +273,7 @@ bool CSVRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns & columns,
             return false;
         }
 
+        skipWhitespacesAndTabs(in);
         if (column_indexes_for_input_fields[file_column].has_value())
         {
             const auto & header = getPort().getHeader();
@@ -289,6 +290,7 @@ bool CSVRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns & columns,
             if (!deserializeFieldAndPrintDiagnosticInfo(skipped_column_str, skipped_column_type, *skipped_column, out, file_column))
                 return false;
         }
+        skipWhitespacesAndTabs(in);
 
         /// Delimiters
         if (file_column + 1 == column_indexes_for_input_fields.size())
@@ -351,12 +353,8 @@ void CSVRowInputFormat::syncAfterError()
     skipToNextLineOrEOF(in);
 }
 
-void CSVRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                            ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos)
+void CSVRowInputFormat::tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column)
 {
-    skipWhitespacesAndTabs(in);
-    prev_pos = in.position();
-
     if (column_indexes_for_input_fields[file_column])
     {
         const bool is_last_file_column = file_column + 1 == column_indexes_for_input_fields.size();
@@ -367,9 +365,6 @@ void CSVRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & 
         String tmp;
         readCSVString(tmp, in, format_settings.csv);
     }
-
-    curr_pos = in.position();
-    skipWhitespacesAndTabs(in);
 }
 
 bool CSVRowInputFormat::readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column)

src/Processors/Formats/Impl/CSVRowInputFormat.h
@@ -55,11 +55,10 @@ private:
     void addInputColumn(const String & column_name);
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                             ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t, ReadBuffer::Position pos) override
     {
-        return *pos != '\n' && *pos != '\r' && *pos != format_settings.csv.delimiter;
+        return *pos != '\n' && *pos != '\r' && *pos != format_settings.csv.delimiter && *pos != ' ' && *pos != '\t';
     }
 
     bool readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column);

src/Processors/Formats/Impl/TabSeparatedRowInputFormat.cpp
@@ -318,10 +318,8 @@ bool TabSeparatedRowInputFormat::parseRowAndPrintDiagnosticInfo(MutableColumns &
     return true;
 }
 
-void TabSeparatedRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                                     ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos)
+void TabSeparatedRowInputFormat::tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column)
 {
-    prev_pos = in.position();
     if (column_indexes_for_input_fields[file_column])
     {
         const bool is_last_file_column = file_column + 1 == column_indexes_for_input_fields.size();
@@ -332,7 +330,6 @@ void TabSeparatedRowInputFormat::tryDeserializeFiled(const DataTypePtr & type, I
         NullSink null_sink;
         readEscapedStringInto(null_sink, in);
     }
-    curr_pos = in.position();
 }
 
 void TabSeparatedRowInputFormat::syncAfterError()

src/Processors/Formats/Impl/TabSeparatedRowInputFormat.h
@@ -50,8 +50,7 @@ private:
     void fillUnreadColumnsWithDefaults(MutableColumns & columns, RowReadExtension & row_read_extension);
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                             ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t, ReadBuffer::Position pos) override { return *pos != '\n' && *pos != '\t'; }
 };
 

src/Processors/Formats/Impl/TemplateRowInputFormat.h
@@ -42,8 +42,7 @@ private:
     [[noreturn]] void throwUnexpectedEof();
 
     bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
-    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column, ReadBuffer::Position & prev_pos,
-                             ReadBuffer::Position & curr_pos) override;
+    void tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column) override;
     bool isGarbageAfterField(size_t after_col_idx, ReadBuffer::Position pos) override;
     void writeErrorStringForWrongDelimiter(WriteBuffer & out, const String & description, const String & delim);
 

src/Processors/Formats/RowInputFormatWithDiagnosticInfo.h
@@ -24,8 +24,7 @@ protected:
                                                 WriteBuffer & out, size_t file_column);
 
     virtual bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) = 0;
-    virtual void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t file_column,
-                                     ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) = 0;
+    virtual void tryDeserializeField(const DataTypePtr & type, IColumn & column, size_t file_column) = 0;
     virtual bool isGarbageAfterField(size_t after_input_pos_idx, ReadBuffer::Position pos) = 0;
 
     /// For convenient diagnostics in case of an error.