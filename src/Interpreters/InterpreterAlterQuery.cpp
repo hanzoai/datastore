ywandy/ClickHouse

Add drop replica alter support

src/Interpreters/InterpreterAlterQuery.cpp
@@ -9,6 +9,8 @@
 #include <Storages/AlterCommands.h>
 #include <Storages/MutationCommands.h>
 #include <Storages/PartitionCommands.h>
+#include <Storages/ReplicaCommands.h>
+#include <Storages/StorageReplicatedMergeTree.h>
 #include <Storages/LiveView/LiveViewCommands.h>
 #include <Storages/LiveView/StorageLiveView.h>
 #include <Access/AccessRightsElement.h>
@@ -54,6 +56,7 @@ BlockIO InterpreterAlterQuery::execute()
 
     AlterCommands alter_commands;
     PartitionCommands partition_commands;
+    ReplicaCommands replica_commands;
     MutationCommands mutation_commands;
     LiveViewCommands live_view_commands;
     for (ASTAlterCommand * command_ast : alter.command_list->commands)
@@ -67,6 +70,10 @@ BlockIO InterpreterAlterQuery::execute()
                 throw DB::Exception("Cannot execute query: DROP DETACHED PART is disabled "
                                     "(see allow_drop_detached setting)", ErrorCodes::SUPPORT_IS_DISABLED);
             partition_commands.emplace_back(std::move(*partition_command));
+        }
+        else if (auto replica_command = ReplicaCommand::parse(command_ast))
+        {
+            replica_commands.emplace_back(std::move(*replica_command));
         }
         else if (auto mut_command = MutationCommand::parse(command_ast))
         {
@@ -93,6 +100,18 @@ BlockIO InterpreterAlterQuery::execute()
     {
         partition_commands.validate(*table);
         table->alterPartition(query_ptr, partition_commands, context);
+    }
+
+    if (!replica_commands.empty())
+    {
+        replica_commands.validate(*table);
+        auto replicate_table = std::dynamic_pointer_cast<StorageReplicatedMergeTree>(table);
+
+        auto table_lock_holder = table->lockAlterIntention(context.getCurrentQueryId());
+        for (auto & command : replica_commands)
+        {   
+            replicate_table->dropReplica(table_lock_holder, command.replica_name);
+        }
     }
 
     if (!live_view_commands.empty())
@@ -226,6 +245,11 @@ AccessRightsElements InterpreterAlterQuery::getRequiredAccess() const
             case ASTAlterCommand::ATTACH_PARTITION:
             {
                 required_access.emplace_back(AccessType::INSERT, alter.database, alter.table);
+                break;
+            }
+            case ASTAlterCommand::DROP_REPLICA:
+            {
+                required_access.emplace_back(AccessType::DELETE, alter.database, alter.table);
                 break;
             }
             case ASTAlterCommand::DROP_PARTITION: [[fallthrough]];

hanzoai/datastore

src/Interpreters/InterpreterAlterQuery.cpp
@@ -9,6 +9,8 @@
 #include <Storages/AlterCommands.h>
 #include <Storages/MutationCommands.h>
 #include <Storages/PartitionCommands.h>
+#include <Storages/ReplicaCommands.h>
+#include <Storages/StorageReplicatedMergeTree.h>
 #include <Storages/LiveView/LiveViewCommands.h>
 #include <Storages/LiveView/StorageLiveView.h>
 #include <Access/AccessRightsElement.h>
@@ -52,6 +54,7 @@ BlockIO InterpreterAlterQuery::execute()
 
     AlterCommands alter_commands;
     PartitionCommands partition_commands;
+    ReplicaCommands replica_commands;
     MutationCommands mutation_commands;
     LiveViewCommands live_view_commands;
     for (ASTAlterCommand * command_ast : alter.command_list->commands)
@@ -65,6 +68,10 @@ BlockIO InterpreterAlterQuery::execute()
                 throw DB::Exception("Cannot execute query: DROP DETACHED PART is disabled "
                                     "(see allow_drop_detached setting)", ErrorCodes::SUPPORT_IS_DISABLED);
             partition_commands.emplace_back(std::move(*partition_command));
+        }
+        else if (auto replica_command = ReplicaCommand::parse(command_ast))
+        {
+            replica_commands.emplace_back(std::move(*replica_command));
         }
         else if (auto mut_command = MutationCommand::parse(command_ast))
         {
@@ -92,6 +99,18 @@ BlockIO InterpreterAlterQuery::execute()
     if (!partition_commands.empty())
     {
         table->alterPartition(query_ptr, partition_commands, context);
+    }
+
+    if (!replica_commands.empty())
+    {
+        replica_commands.validate(*table);
+        auto replicate_table = std::dynamic_pointer_cast<StorageReplicatedMergeTree>(table);
+
+        auto table_lock_holder = table->lockAlterIntention(context.getCurrentQueryId());
+        for (auto & command : replica_commands)
+        {   
+            replicate_table->dropReplica(table_lock_holder, command.replica_name);
+        }
     }
 
     if (!live_view_commands.empty())
@@ -271,6 +290,11 @@ AccessRightsElements InterpreterAlterQuery::getRequiredAccessForCommand(const AS
         case ASTAlterCommand::FREEZE_ALL:
         {
             required_access.emplace_back(AccessType::ALTER_FREEZE_PARTITION, database, table);
+            break;
+        }
+        case ASTAlterCommand::DROP_REPLICA:
+        {
+            required_access.emplace_back(AccessType::ALTER_DELETE, alter.database, alter.table);
             break;
         }
         case ASTAlterCommand::MODIFY_QUERY: