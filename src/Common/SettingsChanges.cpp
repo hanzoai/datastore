vitlibar/ClickHouse

Make SettingsChanges a class.

src/Common/SettingsChanges.cpp
@@ -1,0 +1,50 @@
+#include <Common/SettingsChanges.h>
+
+
+namespace DB
+{
+namespace
+{
+    SettingChange * find(SettingsChanges & changes, const std::string_view & name)
+    {
+        auto it = std::find_if(changes.begin(), changes.end(), [&name](const SettingChange & change) { return change.name == name; });
+        if (it == changes.end())
+            return nullptr;
+        return &*it;
+    }
+
+    const SettingChange * find(const SettingsChanges & changes, const std::string_view & name)
+    {
+        auto it = std::find_if(changes.begin(), changes.end(), [&name](const SettingChange & change) { return change.name == name; });
+        if (it == changes.end())
+            return nullptr;
+        return &*it;
+    }
+}
+
+bool SettingsChanges::tryGet(const std::string_view & name, Field & result) const
+{
+    auto * change = find(*this, name);
+    if (!change)
+        return false;
+    result = change->value;
+    return true;
+}
+
+const Field * SettingsChanges::tryGet(const std::string_view & name) const
+{
+    auto * change = find(*this, name);
+    if (!change)
+        return nullptr;
+    return &change->value;
+}
+
+Field * SettingsChanges::tryGet(const std::string_view & name)
+{
+    auto * change = find(*this, name);
+    if (!change)
+        return nullptr;
+    return &change->value;
+}
+
+}

src/Common/SettingsChanges.h
@@ -5,21 +5,28 @@
 
 namespace DB
 {
-
 struct SettingChange
 {
     String name;
     Field value;
-    SettingChange() {}
 
-    SettingChange(const String & name_, const Field value_)
-    : name(name_)
-    , value(value_) {}
+    SettingChange() {}
+    SettingChange(const std::string_view & name_, const Field & value_) : name(name_), value(value_) {}
+    SettingChange(const std::string_view & name_, Field && value_) : name(name_), value(std::move(value_)) {}
 
     friend bool operator ==(const SettingChange & lhs, const SettingChange & rhs) { return (lhs.name == rhs.name) && (lhs.value == rhs.value); }
     friend bool operator !=(const SettingChange & lhs, const SettingChange & rhs) { return !(lhs == rhs); }
 };
 
-using SettingsChanges = std::vector<SettingChange>;
+
+class SettingsChanges : public std::vector<SettingChange>
+{
+public:
+    using std::vector<SettingChange>::vector;
+
+    bool tryGet(const std::string_view & name, Field & value) const;
+    const Field * tryGet(const std::string_view & name) const;
+    Field * tryGet(const std::string_view & name);
+};
 
 }

hanzoai/datastore

src/Common/SettingsChanges.cpp
@@ -1,0 +1,50 @@
+#include <Common/SettingsChanges.h>
+
+
+namespace DB
+{
+namespace
+{
+    SettingChange * find(SettingsChanges & changes, const std::string_view & name)
+    {
+        auto it = std::find_if(changes.begin(), changes.end(), [&name](const SettingChange & change) { return change.name == name; });
+        if (it == changes.end())
+            return nullptr;
+        return &*it;
+    }
+
+    const SettingChange * find(const SettingsChanges & changes, const std::string_view & name)
+    {
+        auto it = std::find_if(changes.begin(), changes.end(), [&name](const SettingChange & change) { return change.name == name; });
+        if (it == changes.end())
+            return nullptr;
+        return &*it;
+    }
+}
+
+bool SettingsChanges::tryGet(const std::string_view & name, Field & out_value) const
+{
+    const auto * change = find(*this, name);
+    if (!change)
+        return false;
+    out_value = change->value;
+    return true;
+}
+
+const Field * SettingsChanges::tryGet(const std::string_view & name) const
+{
+    const auto * change = find(*this, name);
+    if (!change)
+        return nullptr;
+    return &change->value;
+}
+
+Field * SettingsChanges::tryGet(const std::string_view & name)
+{
+    auto * change = find(*this, name);
+    if (!change)
+        return nullptr;
+    return &change->value;
+}
+
+}

src/Common/SettingsChanges.h
@@ -5,21 +5,28 @@
 
 namespace DB
 {
-
 struct SettingChange
 {
     String name;
     Field value;
-    SettingChange() {}
 
-    SettingChange(const String & name_, const Field value_)
-    : name(name_)
-    , value(value_) {}
+    SettingChange() {}
+    SettingChange(const std::string_view & name_, const Field & value_) : name(name_), value(value_) {}
+    SettingChange(const std::string_view & name_, Field && value_) : name(name_), value(std::move(value_)) {}
 
     friend bool operator ==(const SettingChange & lhs, const SettingChange & rhs) { return (lhs.name == rhs.name) && (lhs.value == rhs.value); }
     friend bool operator !=(const SettingChange & lhs, const SettingChange & rhs) { return !(lhs == rhs); }
 };
 
-using SettingsChanges = std::vector<SettingChange>;
+
+class SettingsChanges : public std::vector<SettingChange>
+{
+public:
+    using std::vector<SettingChange>::vector;
+
+    bool tryGet(const std::string_view & name, Field & out_value) const;
+    const Field * tryGet(const std::string_view & name) const;
+    Field * tryGet(const std::string_view & name);
+};
 
 }