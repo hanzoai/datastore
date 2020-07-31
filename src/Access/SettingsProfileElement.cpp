vitlibar/ClickHouse

Use names of the settings instead of their indices in SettingsConstraints and settings profiles.

src/Access/SettingsProfileElement.cpp
@@ -35,18 +35,18 @@ void SettingsProfileElement::init(const ASTSettingsProfileElement & ast, const A
 
     if (!ast.setting_name.empty())
     {
-        setting_index = Settings::findIndexStrict(ast.setting_name);
+        setting_name = ast.setting_name;
         value = ast.value;
         min_value = ast.min_value;
         max_value = ast.max_value;
         readonly = ast.readonly;
 
         if (!value.isNull())
-            value = Settings::valueToCorrespondingType(setting_index, value);
+            value = Settings::valueToCorrespondingType(setting_name, value);
         if (!min_value.isNull())
-            min_value = Settings::valueToCorrespondingType(setting_index, min_value);
+            min_value = Settings::valueToCorrespondingType(setting_name, min_value);
         if (!max_value.isNull())
-            max_value = Settings::valueToCorrespondingType(setting_index, max_value);
+            max_value = Settings::valueToCorrespondingType(setting_name, max_value);
     }
 }
 
@@ -59,9 +59,7 @@ std::shared_ptr<ASTSettingsProfileElement> SettingsProfileElement::toAST() const
     if (parent_profile)
         ast->parent_profile = ::DB::toString(*parent_profile);
 
-    if (setting_index != static_cast<size_t>(-1))
-        ast->setting_name = Settings::getName(setting_index).toString();
-
+    ast->setting_name = setting_name;
     ast->value = value;
     ast->min_value = min_value;
     ast->max_value = max_value;
@@ -82,9 +80,7 @@ std::shared_ptr<ASTSettingsProfileElement> SettingsProfileElement::toASTWithName
             ast->parent_profile = *parent_profile_name;
     }
 
-    if (setting_index != static_cast<size_t>(-1))
-        ast->setting_name = Settings::getName(setting_index).toString();
-
+    ast->setting_name = setting_name;
     ast->value = value;
     ast->min_value = min_value;
     ast->max_value = max_value;
@@ -135,8 +131,8 @@ Settings SettingsProfileElements::toSettings() const
     Settings res;
     for (const auto & elem : *this)
     {
-        if ((elem.setting_index != static_cast<size_t>(-1)) && !elem.value.isNull())
-            res.set(elem.setting_index, elem.value);
+        if (!elem.setting_name.empty() && !elem.value.isNull())
+            res.set(elem.setting_name, elem.value);
     }
     return res;
 }
@@ -146,8 +142,8 @@ SettingsChanges SettingsProfileElements::toSettingsChanges() const
     SettingsChanges res;
     for (const auto & elem : *this)
     {
-        if ((elem.setting_index != static_cast<size_t>(-1)) && !elem.value.isNull())
-            res.push_back({Settings::getName(elem.setting_index).toString(), elem.value});
+        if (!elem.setting_name.empty() && !elem.value.isNull())
+            res.push_back({elem.setting_name, elem.value});
     }
     return res;
 }
@@ -157,14 +153,14 @@ SettingsConstraints SettingsProfileElements::toSettingsConstraints() const
     SettingsConstraints res;
     for (const auto & elem : *this)
     {
-        if (elem.setting_index != static_cast<size_t>(-1))
+        if (!elem.setting_name.empty())
         {
             if (!elem.min_value.isNull())
-                res.setMinValue(elem.setting_index, elem.min_value);
+                res.setMinValue(elem.setting_name, elem.min_value);
             if (!elem.max_value.isNull())
-                res.setMaxValue(elem.setting_index, elem.max_value);
+                res.setMaxValue(elem.setting_name, elem.max_value);
             if (elem.readonly)
-                res.setReadOnly(elem.setting_index, *elem.readonly);
+                res.setReadOnly(elem.setting_name, *elem.readonly);
         }
     }
     return res;

src/Access/UsersConfigAccessStorage.cpp
@@ -4,9 +4,9 @@
 #include <Access/User.h>
 #include <Access/SettingsProfile.h>
 #include <Dictionaries/IDictionary.h>
-#include <Core/Settings.h>
 #include <Common/StringUtils/StringUtils.h>
 #include <Common/quoteString.h>
+#include <Core/Settings.h>
 #include <Poco/Util/AbstractConfiguration.h>
 #include <Poco/MD5Engine.h>
 #include <common/logger_useful.h>
@@ -348,26 +348,25 @@ namespace
                                                      const String & path_to_constraints)
     {
         SettingsProfileElements profile_elements;
-        Poco::Util::AbstractConfiguration::Keys names;
-        config.keys(path_to_constraints, names);
-        for (const String & name : names)
+        Poco::Util::AbstractConfiguration::Keys keys;
+        config.keys(path_to_constraints, keys);
+        for (const String & setting_name : keys)
         {
             SettingsProfileElement profile_element;
-            size_t setting_index = Settings::findIndexStrict(name);
-            profile_element.setting_index = setting_index;
+            profile_element.setting_name = setting_name;
             Poco::Util::AbstractConfiguration::Keys constraint_types;
-            String path_to_name = path_to_constraints + "." + name;
+            String path_to_name = path_to_constraints + "." + setting_name;
             config.keys(path_to_name, constraint_types);
             for (const String & constraint_type : constraint_types)
             {
                 if (constraint_type == "min")
-                    profile_element.min_value = Settings::valueToCorrespondingType(setting_index, config.getString(path_to_name + "." + constraint_type));
+                    profile_element.min_value = Settings::valueToCorrespondingType(setting_name, config.getString(path_to_name + "." + constraint_type));
                 else if (constraint_type == "max")
-                    profile_element.max_value = Settings::valueToCorrespondingType(setting_index, config.getString(path_to_name + "." + constraint_type));
+                    profile_element.max_value = Settings::valueToCorrespondingType(setting_name, config.getString(path_to_name + "." + constraint_type));
                 else if (constraint_type == "readonly")
                     profile_element.readonly = true;
                 else
-                    throw Exception("Setting " + constraint_type + " value for " + name + " isn't supported", ErrorCodes::NOT_IMPLEMENTED);
+                    throw Exception("Setting " + constraint_type + " value for " + setting_name + " isn't supported", ErrorCodes::NOT_IMPLEMENTED);
             }
             profile_elements.push_back(std::move(profile_element));
         }
@@ -402,10 +401,10 @@ namespace
                 continue;
             }
 
+            const auto & setting_name = key;
             SettingsProfileElement profile_element;
-            size_t setting_index = Settings::findIndexStrict(key);
-            profile_element.setting_index = setting_index;
-            profile_element.value = Settings::valueToCorrespondingType(setting_index, config.getString(profile_config + "." + key));
+            profile_element.setting_name = setting_name;
+            profile_element.value = Settings::valueToCorrespondingType(setting_name, config.getString(profile_config + "." + key));
             profile->elements.emplace_back(std::move(profile_element));
         }
 

hanzoai/datastore

src/Access/SettingsProfileElement.cpp
@@ -36,18 +36,18 @@ void SettingsProfileElement::init(const ASTSettingsProfileElement & ast, const A
 
     if (!ast.setting_name.empty())
     {
-        setting_index = Settings::findIndexStrict(ast.setting_name);
+        setting_name = ast.setting_name;
         value = ast.value;
         min_value = ast.min_value;
         max_value = ast.max_value;
         readonly = ast.readonly;
 
         if (!value.isNull())
-            value = Settings::castValue(setting_index, value);
+            value = Settings::castValue(setting_name, value);
         if (!min_value.isNull())
-            min_value = Settings::castValue(setting_index, min_value);
+            min_value = Settings::castValue(setting_name, min_value);
         if (!max_value.isNull())
-            max_value = Settings::castValue(setting_index, max_value);
+            max_value = Settings::castValue(setting_name, max_value);
     }
 }
 
@@ -60,9 +60,7 @@ std::shared_ptr<ASTSettingsProfileElement> SettingsProfileElement::toAST() const
     if (parent_profile)
         ast->parent_profile = ::DB::toString(*parent_profile);
 
-    if (setting_index != static_cast<size_t>(-1))
-        ast->setting_name = Settings::getName(setting_index).toString();
-
+    ast->setting_name = setting_name;
     ast->value = value;
     ast->min_value = min_value;
     ast->max_value = max_value;
@@ -83,9 +81,7 @@ std::shared_ptr<ASTSettingsProfileElement> SettingsProfileElement::toASTWithName
             ast->parent_profile = *parent_profile_name;
     }
 
-    if (setting_index != static_cast<size_t>(-1))
-        ast->setting_name = Settings::getName(setting_index).toString();
-
+    ast->setting_name = setting_name;
     ast->value = value;
     ast->min_value = min_value;
     ast->max_value = max_value;
@@ -136,8 +132,8 @@ Settings SettingsProfileElements::toSettings() const
     Settings res;
     for (const auto & elem : *this)
     {
-        if ((elem.setting_index != static_cast<size_t>(-1)) && !elem.value.isNull())
-            res.set(elem.setting_index, elem.value);
+        if (!elem.setting_name.empty() && !elem.value.isNull())
+            res.set(elem.setting_name, elem.value);
     }
     return res;
 }
@@ -147,8 +143,8 @@ SettingsChanges SettingsProfileElements::toSettingsChanges() const
     SettingsChanges res;
     for (const auto & elem : *this)
     {
-        if ((elem.setting_index != static_cast<size_t>(-1)) && !elem.value.isNull())
-            res.push_back({Settings::getName(elem.setting_index).toString(), elem.value});
+        if (!elem.setting_name.empty() && !elem.value.isNull())
+            res.push_back({elem.setting_name, elem.value});
     }
     return res;
 }
@@ -158,14 +154,14 @@ SettingsConstraints SettingsProfileElements::toSettingsConstraints() const
     SettingsConstraints res;
     for (const auto & elem : *this)
     {
-        if (elem.setting_index != static_cast<size_t>(-1))
+        if (!elem.setting_name.empty())
         {
             if (!elem.min_value.isNull())
-                res.setMinValue(elem.setting_index, elem.min_value);
+                res.setMinValue(elem.setting_name, elem.min_value);
             if (!elem.max_value.isNull())
-                res.setMaxValue(elem.setting_index, elem.max_value);
+                res.setMaxValue(elem.setting_name, elem.max_value);
             if (elem.readonly)
-                res.setReadOnly(elem.setting_index, *elem.readonly);
+                res.setReadOnly(elem.setting_name, *elem.readonly);
         }
     }
     return res;

src/Access/UsersConfigAccessStorage.cpp
@@ -4,9 +4,9 @@
 #include <Access/User.h>
 #include <Access/SettingsProfile.h>
 #include <Dictionaries/IDictionary.h>
-#include <Core/Settings.h>
 #include <Common/StringUtils/StringUtils.h>
 #include <Common/quoteString.h>
+#include <Core/Settings.h>
 #include <Poco/Util/AbstractConfiguration.h>
 #include <Poco/MD5Engine.h>
 #include <common/logger_useful.h>
@@ -362,26 +362,25 @@ namespace
                                                      const String & path_to_constraints)
     {
         SettingsProfileElements profile_elements;
-        Poco::Util::AbstractConfiguration::Keys names;
-        config.keys(path_to_constraints, names);
-        for (const String & name : names)
+        Poco::Util::AbstractConfiguration::Keys keys;
+        config.keys(path_to_constraints, keys);
+        for (const String & setting_name : keys)
         {
             SettingsProfileElement profile_element;
-            size_t setting_index = Settings::findIndexStrict(name);
-            profile_element.setting_index = setting_index;
+            profile_element.setting_name = setting_name;
             Poco::Util::AbstractConfiguration::Keys constraint_types;
-            String path_to_name = path_to_constraints + "." + name;
+            String path_to_name = path_to_constraints + "." + setting_name;
             config.keys(path_to_name, constraint_types);
             for (const String & constraint_type : constraint_types)
             {
                 if (constraint_type == "min")
-                    profile_element.min_value = Settings::stringToValue(setting_index, config.getString(path_to_name + "." + constraint_type));
+                    profile_element.min_value = Settings::stringToValue(setting_name, config.getString(path_to_name + "." + constraint_type));
                 else if (constraint_type == "max")
-                    profile_element.max_value = Settings::stringToValue(setting_index, config.getString(path_to_name + "." + constraint_type));
+                    profile_element.max_value = Settings::stringToValue(setting_name, config.getString(path_to_name + "." + constraint_type));
                 else if (constraint_type == "readonly")
                     profile_element.readonly = true;
                 else
-                    throw Exception("Setting " + constraint_type + " value for " + name + " isn't supported", ErrorCodes::NOT_IMPLEMENTED);
+                    throw Exception("Setting " + constraint_type + " value for " + setting_name + " isn't supported", ErrorCodes::NOT_IMPLEMENTED);
             }
             profile_elements.push_back(std::move(profile_element));
         }
@@ -416,10 +415,11 @@ namespace
                 continue;
             }
 
+            const auto & setting_name = key;
             SettingsProfileElement profile_element;
             size_t setting_index = Settings::findIndexStrict(key);
-            profile_element.setting_index = setting_index;
-            profile_element.value = Settings::stringToValue(setting_index, config.getString(profile_config + "." + key));
+            profile_element.setting_name = setting_name;
+            profile_element.value = Settings::stringToValue(setting_name, config.getString(profile_config + "." + key));
             profile->elements.emplace_back(std::move(profile_element));
         }
 