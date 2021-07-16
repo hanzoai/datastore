vitlibar/ClickHouse

Add comments for encrypted disks; some improvements in code.

src/IO/FileEncryptionCommon.h
@@ -16,87 +16,80 @@ class WriteBuffer;
 namespace FileEncryption
 {
 
-constexpr size_t kIVSize = sizeof(UInt128);
-
+/// Initialization vector. Its size is always 16 bytes.
 class InitVector
 {
 public:
-    InitVector(const String & iv_);
-    const char * c_str() const;
-    void inc() { ++counter; local.clear(); }
-    void inc(size_t n) { counter += n; local.clear(); }
-    void set(size_t n) { counter = n; local.clear(); }
+    static constexpr const size_t kSize = 16;
+
+    InitVector() = default;
+    explicit InitVector(const UInt128 & counter_) { set(counter_); }
+
+    void set(const UInt128 & counter_) { counter = counter_; }
+    UInt128 toUInt128() const { return counter; }
+
+    void read(ReadBuffer & in);
+    void write(WriteBuffer & out) const;
+
+    /// Write 16 bytes of the counter to a string in big endian order.
+    /// We need big endian because the used cipher algorithms treat an initialization vector as a counter in big endian.
+    String toBigEndian() const;
+
+    /// Converts a string of 16 bytes length in big endian order to a counter.
+    static InitVector fromBigEndian(const String & str_);
+
+    /// Adds a specified offset to the counter.
+    InitVector & operator++() { ++counter; return *this; }
+    InitVector operator++(int) { InitVector res = *this; ++counter; return res; }
+    InitVector & operator+=(size_t offset) { counter += offset; return *this; }
+    InitVector operator+(size_t offset) const { InitVector res = *this; return res += offset; }
+
+    /// Generates a random initialization vector.
+    static InitVector random();
 
 private:
-    UInt128 iv;
     UInt128 counter = 0;
-    mutable String local;
 };
 
 
-class EncryptionKey
+/// Encrypts or decrypts data.
+class Encryptor
 {
 public:
-    EncryptionKey(const String & key_) : key(key_) { }
-    size_t size() const { return key.size(); }
-    const char * c_str() const { return key.c_str(); }
+    /// The `key` should have length 128 or 192 or 256.
+    /// According to the key's length aes_128_ctr or aes_192_ctr or aes_256_ctr will be used for encryption.
+    /// We chose to use CTR cipther algoritms because they have the following important features:
+    /// - No right padding, so we can append encrypted files without deciphering;
+    /// - One byte is always ciphered as one byte, so we get random access to encrypted files easily.
+    Encryptor(const String & key_, const InitVector & iv_);
+
+    /// Sets the current position in the data stream from the very beginning of data.
+    /// It affects how the data will be encrypted or decrypted because
+    /// the initialization vector is increased by an index of the current block
+    /// and the index of the current block is calculated from this offset.
+    void setOffset(size_t offset_) { offset = offset_; }
+
+    /// Encrypts some data.
+    /// Also the function moves `offset` by `size` (for successive encryptions).
+    void encrypt(const char * data, size_t size, WriteBuffer & out);
+
+    /// Decrypts some data.
+    /// The used cipher algorithms generate the same number of bytes in output as it were in input,
+    /// so the function always writes `size` bytes of the plaintext to `out`.
+    /// Also the function moves `offset` by `size` (for successive decryptions).
+    void decrypt(const char * data, size_t size, char * out);
+
+    /// Checks whether a passed key length is supported, i.e. it has length 128 or 192 or 256.
+    static void checkKeyLengthIsSupported(size_t key_length);
 
 private:
-    String key;
-};
-
-
-class Encryption
-{
-public:
-    Encryption(const String & iv_, const EncryptionKey & key_, size_t offset_);
-
-protected:
-    size_t blockOffset(size_t pos) const { return pos % block_size; }
-    size_t blocks(size_t pos) const { return pos / block_size; }
-    size_t partBlockSize(size_t size, size_t off);
-    const EVP_CIPHER * get() const { return evp_cipher; }
-
+    const String key;
+    const InitVector init_vector;
     const EVP_CIPHER * evp_cipher;
-    const String init_vector;
-    const EncryptionKey key;
-    size_t block_size;
 
-    /// absolute offset
+    /// The current position in the data stream from the very beginning of data.
     size_t offset = 0;
 };
-
-
-class Encryptor : public Encryption
-{
-public:
-    using Encryption::Encryption;
-    void encrypt(const char * plaintext, WriteBuffer & buf, size_t size);
-
-private:
-    String encryptPartialBlock(const char * partial_block, size_t size, const InitVector & iv, size_t off) const;
-    String encryptNBytes(const char * data, size_t bytes, const InitVector & iv) const;
-};
-
-
-class Decryptor : public Encryption
-{
-public:
-    Decryptor(const String & iv_, const EncryptionKey & key_) : Encryption(iv_, key_, 0) { }
-    void decrypt(const char * ciphertext, char * buf, size_t size, size_t off);
-
-private:
-    void decryptPartialBlock(char *& to, const char * partial_block, size_t size, const InitVector & iv, size_t off) const;
-    void decryptNBytes(char *& to, const char * data, size_t bytes, const InitVector & iv) const;
-};
-
-
-String readIV(size_t size, ReadBuffer & in);
-String randomString(size_t size);
-void writeIV(const String & iv, WriteBuffer & out);
-size_t cipherKeyLength(const EVP_CIPHER * evp_cipher);
-size_t cipherIVLength(const EVP_CIPHER * evp_cipher);
-const EVP_CIPHER * defaultCipher();
 
 }
 }

hanzoai/datastore

src/IO/FileEncryptionCommon.h
@@ -16,87 +16,82 @@ class WriteBuffer;
 namespace FileEncryption
 {
 
-constexpr size_t kIVSize = sizeof(UInt128);
-
+/// Initialization vector. Its size is always 16 bytes.
 class InitVector
 {
 public:
-    InitVector(const String & iv_);
-    const String & str() const;
-    void inc() { ++counter; }
-    void inc(size_t n) { counter += n; }
-    void set(size_t n) { counter = n; }
+    static constexpr const size_t kSize = 16;
+
+    InitVector() = default;
+    explicit InitVector(const UInt128 & counter_) { set(counter_); }
+
+    void set(const UInt128 & counter_) { counter = counter_; }
+    UInt128 get() const { return counter; }
+
+    void read(ReadBuffer & in);
+    void write(WriteBuffer & out) const;
+
+    /// Write 16 bytes of the counter to a string in big endian order.
+    /// We need big endian because the used cipher algorithms treat an initialization vector as a counter in big endian.
+    String toString() const;
+
+    /// Converts a string of 16 bytes length in big endian order to a counter.
+    static InitVector fromString(const String & str_);
+
+    /// Adds a specified offset to the counter.
+    InitVector & operator++() { ++counter; return *this; }
+    InitVector operator++(int) { InitVector res = *this; ++counter; return res; }
+    InitVector & operator+=(size_t offset) { counter += offset; return *this; }
+    InitVector operator+(size_t offset) const { InitVector res = *this; return res += offset; }
+
+    /// Generates a random initialization vector.
+    static InitVector random();
 
 private:
-    UInt128 iv;
     UInt128 counter = 0;
-    mutable String local;
 };
 
 
-class EncryptionKey
+/// Encrypts or decrypts data.
+class Encryptor
 {
 public:
-    EncryptionKey(const String & key_) : key(key_) { }
-    size_t size() const { return key.size(); }
-    const String & str() const { return key; }
+    /// The `key` should have length 128 or 192 or 256.
+    /// According to the key's length aes_128_ctr or aes_192_ctr or aes_256_ctr will be used for encryption.
+    /// We chose to use CTR cipther algorithms because they have the following features which are important for us:
+    /// - No right padding, so we can append encrypted files without deciphering;
+    /// - One byte is always ciphered as one byte, so we get random access to encrypted files easily.
+    Encryptor(const String & key_, const InitVector & iv_);
+
+    /// Sets the current position in the data stream from the very beginning of data.
+    /// It affects how the data will be encrypted or decrypted because
+    /// the initialization vector is increased by an index of the current block
+    /// and the index of the current block is calculated from this offset.
+    void setOffset(size_t offset_) { offset = offset_; }
+
+    /// Encrypts some data.
+    /// Also the function moves `offset` by `size` (for successive encryptions).
+    void encrypt(const char * data, size_t size, WriteBuffer & out);
+
+    /// Decrypts some data.
+    /// The used cipher algorithms generate the same number of bytes in output as they were in input,
+    /// so the function always writes `size` bytes of the plaintext to `out`.
+    /// Also the function moves `offset` by `size` (for successive decryptions).
+    void decrypt(const char * data, size_t size, char * out);
 
 private:
-    String key;
-};
-
-
-class Encryption
-{
-public:
-    Encryption(const String & iv_, const EncryptionKey & key_, size_t offset_);
-
-protected:
-    size_t blockOffset(size_t pos) const { return pos % block_size; }
-    size_t blocks(size_t pos) const { return pos / block_size; }
-    size_t partBlockSize(size_t size, size_t off) const;
-    const EVP_CIPHER * get() const { return evp_cipher; }
-
+    const String key;
+    const InitVector init_vector;
     const EVP_CIPHER * evp_cipher;
-    const String init_vector;
-    const EncryptionKey key;
-    size_t block_size;
 
-    /// absolute offset
+    /// The current position in the data stream from the very beginning of data.
     size_t offset = 0;
 };
 
 
-class Encryptor : public Encryption
-{
-public:
-    using Encryption::Encryption;
-    void encrypt(const char * plaintext, WriteBuffer & buf, size_t size);
-
-private:
-    String encryptPartialBlock(const char * partial_block, size_t size, const InitVector & iv, size_t off) const;
-    String encryptNBytes(const char * data, size_t bytes, const InitVector & iv) const;
-};
-
-
-class Decryptor : public Encryption
-{
-public:
-    Decryptor(const String & iv_, const EncryptionKey & key_) : Encryption(iv_, key_, 0) { }
-    void decrypt(const char * ciphertext, char * buf, size_t size, size_t off);
-
-private:
-    void decryptPartialBlock(char *& to, const char * partial_block, size_t size, const InitVector & iv, size_t off) const;
-    void decryptNBytes(char *& to, const char * data, size_t bytes, const InitVector & iv) const;
-};
-
-
-String readIV(size_t size, ReadBuffer & in);
-String randomString(size_t size);
-void writeIV(const String & iv, WriteBuffer & out);
-size_t cipherKeyLength(const EVP_CIPHER * evp_cipher);
-size_t cipherIVLength(const EVP_CIPHER * evp_cipher);
-const EVP_CIPHER * defaultCipher();
+/// Checks whether a passed key length is supported, i.e.
+/// whether its length is 128 or 192 or 256 bits (16 or 24 or 32 bytes).
+bool isKeyLengthSupported(size_t key_length);
 
 }
 }