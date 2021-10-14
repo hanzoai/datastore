Avogar/ClickHouse

Add CapnProto output format, refactor CapnProto input format

src/Formats/registerFormats.cpp
@@ -71,6 +71,7 @@ void registerOutputFormatProcessorNull(FormatFactory & factory);
 void registerOutputFormatProcessorMySQLWire(FormatFactory & factory);
 void registerOutputFormatProcessorMarkdown(FormatFactory & factory);
 void registerOutputFormatProcessorPostgreSQLWire(FormatFactory & factory);
+void registerOutputFormatProcessorsCapnProto(FormatFactory & factory);
 
 /// Input only formats.
 
@@ -147,6 +148,7 @@ void registerFormats()
     registerOutputFormatProcessorMySQLWire(factory);
     registerOutputFormatProcessorMarkdown(factory);
     registerOutputFormatProcessorPostgreSQLWire(factory);
+    registerOutputFormatProcessorsCapnProto(factory);
 
     registerInputFormatProcessorRegexp(factory);
     registerInputFormatProcessorJSONAsString(factory);

hanzoai/datastore

src/Formats/registerFormats.cpp
@@ -67,6 +67,7 @@ void registerOutputFormatNull(FormatFactory & factory);
 void registerOutputFormatMySQLWire(FormatFactory & factory);
 void registerOutputFormatMarkdown(FormatFactory & factory);
 void registerOutputFormatPostgreSQLWire(FormatFactory & factory);
+void registerOutputFormatCapnProto(FormatFactory & factory);
 
 /// Input only formats.
 
@@ -139,6 +140,7 @@ void registerFormats()
     registerOutputFormatMySQLWire(factory);
     registerOutputFormatMarkdown(factory);
     registerOutputFormatPostgreSQLWire(factory);
+    registerOutputFormatProcessorsCapnProto(factory);
 
     registerInputFormatRegexp(factory);
     registerInputFormatJSONAsString(factory);