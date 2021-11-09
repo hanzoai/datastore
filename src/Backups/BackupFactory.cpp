ClickHouse/ClickHouse

New syntax for BACKUP/RESTORE: set backup engine explicitly.

src/Backups/BackupFactory.cpp
@@ -1,19 +1,13 @@
 #include <Backups/BackupFactory.h>
-#include <Backups/BackupInDirectory.h>
-#include <Interpreters/Context.h>
-#include <Disks/IVolume.h>
 
 
 namespace DB
 {
 namespace ErrorCodes
 {
-    extern const int BACKUP_NOT_FOUND;
-    extern const int BACKUP_ALREADY_EXISTS;
-    extern const int NOT_ENOUGH_SPACE;
+    extern const int BACKUP_ENGINE_NOT_FOUND;
     extern const int LOGICAL_ERROR;
 }
-
 
 BackupFactory & BackupFactory::instance()
 {
@@ -21,45 +15,27 @@ BackupFactory & BackupFactory::instance()
     return the_instance;
 }
 
-void BackupFactory::setBackupsVolume(VolumePtr backups_volume_)
+BackupMutablePtr BackupFactory::createBackup(const CreateParams & params) const
 {
-    backups_volume = backups_volume_;
+    const String & engine_name = params.backup_info.backup_engine_name;
+    auto it = creators.find(engine_name);
+    if (it == creators.end())
+        throw Exception(ErrorCodes::BACKUP_ENGINE_NOT_FOUND, "Not found backup engine {}", engine_name);
+    return (it->second)(params);
 }
 
-BackupMutablePtr BackupFactory::createBackup(const String & backup_name, UInt64 estimated_backup_size, const BackupPtr & base_backup) const
+void BackupFactory::registerBackupEngine(const String & engine_name, const CreatorFn & creator)
 {
-    if (!backups_volume)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "No backups volume");
-
-    for (const auto & disk : backups_volume->getDisks())
-    {
-        if (disk->exists(backup_name))
-            throw Exception(ErrorCodes::BACKUP_ALREADY_EXISTS, "Backup {} already exists", quoteString(backup_name));
-    }
-
-    auto reservation = backups_volume->reserve(estimated_backup_size);
-    if (!reservation)
-        throw Exception(
-            ErrorCodes::NOT_ENOUGH_SPACE,
-            "Couldn't reserve {} bytes of free space for new backup {}",
-            estimated_backup_size,
-            quoteString(backup_name));
-
-    return std::make_shared<BackupInDirectory>(IBackup::OpenMode::WRITE, reservation->getDisk(), backup_name, base_backup);
+    if (creators.contains(engine_name))
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Backup engine {} was registered twice", engine_name);
+    creators[engine_name] = creator;
 }
 
-BackupPtr BackupFactory::openBackup(const String & backup_name, const BackupPtr & base_backup) const
+void registerBackupEngines(BackupFactory & factory);
+
+BackupFactory::BackupFactory()
 {
-    if (!backups_volume)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "No backups volume");
-
-    for (const auto & disk : backups_volume->getDisks())
-    {
-        if (disk->exists(backup_name))
-            return std::make_shared<BackupInDirectory>(IBackup::OpenMode::READ, disk, backup_name, base_backup);
-    }
-
-    throw Exception(ErrorCodes::BACKUP_NOT_FOUND, "Backup {} not found", quoteString(backup_name));
+    registerBackupEngines(*this);
 }
 
 }

src/Backups/BackupFactory.h
@@ -1,38 +1,46 @@
 #pragma once
 
+#include <Backups/IBackup.h>
+#include <Backups/BackupInfo.h>
 #include <Core/Types.h>
+#include <Parsers/IAST_fwd.h>
 #include <boost/noncopyable.hpp>
 #include <memory>
+#include <optional>
+#include <unordered_map>
 
 
 namespace DB
 {
-class IBackup;
-using BackupPtr = std::shared_ptr<const IBackup>;
-using BackupMutablePtr = std::shared_ptr<IBackup>;
 class Context;
-using ContextMutablePtr = std::shared_ptr<Context>;
-class IVolume;
-using VolumePtr = std::shared_ptr<IVolume>;
-
+using ContextPtr = std::shared_ptr<const Context>;
 
 /// Factory for implementations of the IBackup interface.
 class BackupFactory : boost::noncopyable
 {
 public:
+    using OpenMode = IBackup::OpenMode;
+
+    struct CreateParams
+    {
+        OpenMode open_mode = OpenMode::WRITE;
+        BackupInfo backup_info;
+        std::optional<BackupInfo> base_backup_info;
+        ContextPtr context;
+    };
+
     static BackupFactory & instance();
 
-    /// Must be called to initialize the backup factory.
-    void setBackupsVolume(VolumePtr backups_volume_);
+    /// Creates a new backup or opens it.
+    BackupMutablePtr createBackup(const CreateParams & params) const;
 
-    /// Creates a new backup and open it for writing.
-    BackupMutablePtr createBackup(const String & backup_name, UInt64 estimated_backup_size, const BackupPtr & base_backup = {}) const;
-
-    /// Opens an existing backup for reading.
-    BackupPtr openBackup(const String & backup_name, const BackupPtr & base_backup = {}) const;
+    using CreatorFn = std::function<BackupMutablePtr(const CreateParams & params)>;
+    void registerBackupEngine(const String & engine_name, const CreatorFn & f);
 
 private:
-    VolumePtr backups_volume;
+    BackupFactory();
+
+    std::unordered_map<String, CreatorFn> creators;
 };
 
 }

hanzoai/datastore

src/Backups/BackupFactory.cpp
@@ -1,19 +1,13 @@
 #include <Backups/BackupFactory.h>
-#include <Backups/BackupInDirectory.h>
-#include <Interpreters/Context.h>
-#include <Disks/IVolume.h>
 
 
 namespace DB
 {
 namespace ErrorCodes
 {
-    extern const int BACKUP_NOT_FOUND;
-    extern const int BACKUP_ALREADY_EXISTS;
-    extern const int NOT_ENOUGH_SPACE;
+    extern const int BACKUP_ENGINE_NOT_FOUND;
     extern const int LOGICAL_ERROR;
 }
-
 
 BackupFactory & BackupFactory::instance()
 {
@@ -21,45 +15,27 @@ BackupFactory & BackupFactory::instance()
     return the_instance;
 }
 
-void BackupFactory::setBackupsVolume(VolumePtr backups_volume_)
+BackupMutablePtr BackupFactory::createBackup(const CreateParams & params) const
 {
-    backups_volume = backups_volume_;
+    const String & engine_name = params.backup_info.backup_engine_name;
+    auto it = creators.find(engine_name);
+    if (it == creators.end())
+        throw Exception(ErrorCodes::BACKUP_ENGINE_NOT_FOUND, "Not found backup engine {}", engine_name);
+    return (it->second)(params);
 }
 
-BackupMutablePtr BackupFactory::createBackup(const String & backup_name, UInt64 estimated_backup_size, const BackupPtr & base_backup) const
+void BackupFactory::registerBackupEngine(const String & engine_name, const CreatorFn & creator_fn)
 {
-    if (!backups_volume)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "No backups volume");
-
-    for (const auto & disk : backups_volume->getDisks())
-    {
-        if (disk->exists(backup_name))
-            throw Exception(ErrorCodes::BACKUP_ALREADY_EXISTS, "Backup {} already exists", quoteString(backup_name));
-    }
-
-    auto reservation = backups_volume->reserve(estimated_backup_size);
-    if (!reservation)
-        throw Exception(
-            ErrorCodes::NOT_ENOUGH_SPACE,
-            "Couldn't reserve {} bytes of free space for new backup {}",
-            estimated_backup_size,
-            quoteString(backup_name));
-
-    return std::make_shared<BackupInDirectory>(IBackup::OpenMode::WRITE, reservation->getDisk(), backup_name, base_backup);
+    if (creators.contains(engine_name))
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Backup engine {} was registered twice", engine_name);
+    creators[engine_name] = creator_fn;
 }
 
-BackupPtr BackupFactory::openBackup(const String & backup_name, const BackupPtr & base_backup) const
+void registerBackupEngines(BackupFactory & factory);
+
+BackupFactory::BackupFactory()
 {
-    if (!backups_volume)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "No backups volume");
-
-    for (const auto & disk : backups_volume->getDisks())
-    {
-        if (disk->exists(backup_name))
-            return std::make_shared<BackupInDirectory>(IBackup::OpenMode::READ, disk, backup_name, base_backup);
-    }
-
-    throw Exception(ErrorCodes::BACKUP_NOT_FOUND, "Backup {} not found", quoteString(backup_name));
+    registerBackupEngines(*this);
 }
 
 }

src/Backups/BackupFactory.h
@@ -1,38 +1,46 @@
 #pragma once
 
+#include <Backups/IBackup.h>
+#include <Backups/BackupInfo.h>
 #include <Core/Types.h>
+#include <Parsers/IAST_fwd.h>
 #include <boost/noncopyable.hpp>
 #include <memory>
+#include <optional>
+#include <unordered_map>
 
 
 namespace DB
 {
-class IBackup;
-using BackupPtr = std::shared_ptr<const IBackup>;
-using BackupMutablePtr = std::shared_ptr<IBackup>;
 class Context;
-using ContextMutablePtr = std::shared_ptr<Context>;
-class IVolume;
-using VolumePtr = std::shared_ptr<IVolume>;
-
+using ContextPtr = std::shared_ptr<const Context>;
 
 /// Factory for implementations of the IBackup interface.
 class BackupFactory : boost::noncopyable
 {
 public:
+    using OpenMode = IBackup::OpenMode;
+
+    struct CreateParams
+    {
+        OpenMode open_mode = OpenMode::WRITE;
+        BackupInfo backup_info;
+        std::optional<BackupInfo> base_backup_info;
+        ContextPtr context;
+    };
+
     static BackupFactory & instance();
 
-    /// Must be called to initialize the backup factory.
-    void setBackupsVolume(VolumePtr backups_volume_);
+    /// Creates a new backup or opens it.
+    BackupMutablePtr createBackup(const CreateParams & params) const;
 
-    /// Creates a new backup and open it for writing.
-    BackupMutablePtr createBackup(const String & backup_name, UInt64 estimated_backup_size, const BackupPtr & base_backup = {}) const;
-
-    /// Opens an existing backup for reading.
-    BackupPtr openBackup(const String & backup_name, const BackupPtr & base_backup = {}) const;
+    using CreatorFn = std::function<BackupMutablePtr(const CreateParams & params)>;
+    void registerBackupEngine(const String & engine_name, const CreatorFn & creator_fn);
 
 private:
-    VolumePtr backups_volume;
+    BackupFactory();
+
+    std::unordered_map<String, CreatorFn> creators;
 };
 
 }