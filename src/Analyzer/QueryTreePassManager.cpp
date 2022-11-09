alexeyerm/ClickHouse

Support optimize_syntax_fuse_functions for sum/count/avg via analyzer

src/Analyzer/QueryTreePassManager.cpp
@@ -10,6 +10,7 @@
 #include <Analyzer/Passes/CountDistinctPass.h>
 #include <Analyzer/Passes/CustomizeFunctionsPass.h>
 #include <Analyzer/Passes/OrderByLimitByDuplicateEliminationPass.h>
+#include <Analyzer/Passes/FuseFunctionsPass.h>
 
 #include <IO/WriteHelpers.h>
 #include <IO/Operators.h>
@@ -136,6 +137,9 @@ void addQueryTreePasses(QueryTreePassManager & manager)
 
     manager.addPass(std::make_shared<OrderByTupleEliminationPass>());
     manager.addPass(std::make_shared<OrderByLimitByDuplicateEliminationPass>());
+
+    if (settings.optimize_syntax_fuse_functions)
+        manager.addPass(std::make_shared<FuseFunctionsPass>());
 }
 
 }

tests/queries/0_stateless/01744_fuse_sum_count_aggregate.sql
@@ -1,13 +1,19 @@
+SET use_analyzer = 1;
+
 DROP TABLE IF EXISTS fuse_tbl;
-CREATE TABLE fuse_tbl(a Int8, b Int8) Engine = Log;
-INSERT INTO fuse_tbl SELECT number, number + 1 FROM numbers(1, 20);
+CREATE TABLE fuse_tbl(a Nullable(Int8), b Int8) Engine = Log;
+INSERT INTO fuse_tbl VALUES (1, 1), (2, 2), (NULL, 3);
 
 SET optimize_syntax_fuse_functions = 1;
-SET optimize_fuse_sum_count_avg = 1;
 
-SELECT sum(a), sum(b), count(b) from fuse_tbl;
-EXPLAIN SYNTAX SELECT sum(a), sum(b), count(b) from fuse_tbl;
-SELECT '---------NOT trigger fuse--------';
-SELECT sum(a), avg(b) from fuse_tbl;
-EXPLAIN SYNTAX SELECT sum(a), avg(b) from fuse_tbl;
+SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
+EXPLAIN QUERY TREE run_passes = 1 SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
+
+SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0)) SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0));
+
+SELECT sum(x), count(x), avg(x), toTypeName(sum(x)), toTypeName(count(x)), toTypeName(avg(x)) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(10)) SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(x), count(x), avg(x), toTypeName(sum(x)), toTypeName(count(x)), toTypeName(avg(x)) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(10));
+
 DROP TABLE fuse_tbl;

hanzoai/datastore

src/Analyzer/QueryTreePassManager.cpp
@@ -13,6 +13,7 @@
 #include <Analyzer/Passes/AggregateFunctionsArithmericOperationsPass.h>
 #include <Analyzer/Passes/UniqInjectiveFunctionsEliminationPass.h>
 #include <Analyzer/Passes/OrderByLimitByDuplicateEliminationPass.h>
+#include <Analyzer/Passes/FuseFunctionsPass.h>
 
 #include <IO/WriteHelpers.h>
 #include <IO/Operators.h>
@@ -191,6 +192,9 @@ void addQueryTreePasses(QueryTreePassManager & manager)
 
     manager.addPass(std::make_unique<OrderByTupleEliminationPass>());
     manager.addPass(std::make_unique<OrderByLimitByDuplicateEliminationPass>());
+
+    if (settings.optimize_syntax_fuse_functions)
+        manager.addPass(std::make_unique<FuseFunctionsPass>());
 }
 
 }

tests/queries/0_stateless/01744_fuse_sum_count_aggregate.sql
@@ -1,14 +1,25 @@
+SET allow_experimental_analyzer = 1;
+
 DROP TABLE IF EXISTS fuse_tbl;
-CREATE TABLE fuse_tbl(a Int8, b Int8) Engine = Log;
-INSERT INTO fuse_tbl SELECT number, number + 1 FROM numbers(1, 20);
+CREATE TABLE fuse_tbl(a Nullable(Int8), b Int8) Engine = Log;
+INSERT INTO fuse_tbl VALUES (1, 1), (2, 2), (NULL, 3);
 
 SET optimize_syntax_fuse_functions = 1;
-SET optimize_fuse_sum_count_avg = 1;
 
 SELECT sum(a), sum(b), count(b) from fuse_tbl;
 EXPLAIN SYNTAX SELECT sum(a), sum(b), count(b) from fuse_tbl;
 SELECT '---------NOT trigger fuse--------';
 SELECT sum(a), avg(b) from fuse_tbl;
 EXPLAIN SYNTAX SELECT sum(a), avg(b) from fuse_tbl;
+
+SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
+EXPLAIN QUERY TREE run_passes = 1 SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
+
+SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0)) SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0));
+
+SELECT sum(x), count(x), avg(x), toTypeName(sum(x)), toTypeName(count(x)), toTypeName(avg(x)) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(10)) SETTINGS optimize_syntax_fuse_functions = 0;
+SELECT sum(x), count(x), avg(x), toTypeName(sum(x)), toTypeName(count(x)), toTypeName(avg(x)) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(10));
 
 DROP TABLE fuse_tbl;