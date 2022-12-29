zy-kkk/ClickHouse

Support optimize_or_like_chain in QueryTreePassManager

src/Analyzer/QueryTreePassManager.cpp
@@ -13,6 +13,7 @@
 #include <Analyzer/Passes/AggregateFunctionsArithmericOperationsPass.h>
 #include <Analyzer/Passes/UniqInjectiveFunctionsEliminationPass.h>
 #include <Analyzer/Passes/OrderByLimitByDuplicateEliminationPass.h>
+#include <Analyzer/Passes/ConvertOrLikeChainPass.h>
 
 #include <IO/WriteHelpers.h>
 #include <IO/Operators.h>
@@ -146,6 +147,12 @@ void addQueryTreePasses(QueryTreePassManager & manager)
 
     manager.addPass(std::make_shared<OrderByTupleEliminationPass>());
     manager.addPass(std::make_shared<OrderByLimitByDuplicateEliminationPass>());
+
+    if (settings.optimize_or_like_chain
+        && settings.allow_hyperscan
+        && settings.max_hyperscan_regexp_length == 0
+        && settings.max_hyperscan_regexp_total_length == 0)
+        manager.addPass(std::make_shared<ConvertOrLikeChainPass>());
 }
 
 }

hanzoai/datastore

src/Analyzer/QueryTreePassManager.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <Analyzer/QueryTreePassManager.h>
 
 #include <Analyzer/Passes/QueryAnalysisPass.h>
@@ -15,6 +16,7 @@
 #include <Analyzer/Passes/OrderByLimitByDuplicateEliminationPass.h>
 #include <Analyzer/Passes/FuseFunctionsPass.h>
 #include <Analyzer/Passes/IfTransformStringsToEnumPass.h>
+#include <Analyzer/Passes/ConvertOrLikeChainPass.h>
 
 #include <IO/WriteHelpers.h>
 #include <IO/Operators.h>
@@ -211,6 +213,12 @@ void addQueryTreePasses(QueryTreePassManager & manager)
 
     if (settings.optimize_if_transform_strings_to_enum)
         manager.addPass(std::make_unique<IfTransformStringsToEnumPass>());
+
+    if (settings.optimize_or_like_chain
+        && settings.allow_hyperscan
+        && settings.max_hyperscan_regexp_length == 0
+        && settings.max_hyperscan_regexp_total_length == 0)
+        manager.addPass(std::make_unique<ConvertOrLikeChainPass>());
 }
 
 }