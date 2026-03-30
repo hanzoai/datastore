ClickHouse/ClickHouse

Add grant_scope enum to system.grants (#92835)
This contribution adds a new `grant_scope` Enum8 column to the `system.grants` table to allow users and external integrations to accurately distinguish wildcard prefix grants from exact-match grants.

The Problem:
When wildcard prefix grants (e.g., `GRANT SELECT ON foo.bar*`) and exact grants (`GRANT SELECT ON foo.bar`) coexist, the `system.grants` table previously showed identical rows for both. This created a visibility gap for security auditing and external RBAC synchronization.

The Solution:
Added `grant_scope`, which surfaces the internal wildcard evaluation state as an Enum with three mutually exclusive values:
- 'exact' (0): Exact database and table match.
- 'database_prefix' (1): Database name is a wildcard prefix (e.g., db*.*).
- 'table_prefix' (2): Table name is a wildcard prefix (e.g., foo.bar*).

Design Decisions:
Implemented as a single Enum8 column rather than multiple boolean columns. This conserves horizontal schema space, enforces the mutual exclusivity of the states, and provides a scalable foundation for future wildcard scope expansions (such as column-level prefixes).

Testing:
Added stateless test `02935_system_grants_wildcards` to verify accurate output across all three states.

src/Storages/System/StorageSystemGrants.cpp
@@ -39,6 +39,16 @@ ColumnsDescription StorageSystemGrants::getColumnsDescription()
             "1 — The row describes a partial revoke."
         },
         {"grant_option", std::make_shared<DataTypeUInt8>(), "Permission is granted WITH GRANT OPTION."},
+        {"grant_scope", std::make_shared<DataTypeEnum8>(
+            DataTypeEnum8::Values{
+                {"exact",            0},
+                {"database_prefix",  1},
+                {"table_prefix",     2},
+            }), "Scope type of the grant: "
+                "'exact' — exact database/table match, "
+                "'database_prefix' — database name is a wildcard prefix (e.g. db*.*), "
+                "'table_prefix' — table name is a wildcard prefix (e.g. foo.bar*)."
+        },
     };
 }
 
@@ -68,6 +78,7 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
     auto & column_column_null_map = assert_cast<ColumnNullable &>(*res_columns[column_index++]).getNullMapData();
     auto & column_is_partial_revoke = assert_cast<ColumnUInt8 &>(*res_columns[column_index++]).getData();
     auto & column_grant_option = assert_cast<ColumnUInt8 &>(*res_columns[column_index++]).getData();
+    auto & column_grant_scope = assert_cast<ColumnInt8 &>(*res_columns[column_index++]).getData();
 
     auto add_row = [&](const String & grantee_name,
                        AccessEntityType grantee_type,
@@ -77,7 +88,8 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
                        const String * table,
                        const String * column,
                        bool is_partial_revoke,
-                       bool grant_option)
+                       bool grant_option,
+                       Int8 grant_scope)
     {
         if (grantee_type == AccessEntityType::USER)
         {
@@ -133,6 +145,7 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
 
         column_is_partial_revoke.push_back(is_partial_revoke);
         column_grant_option.push_back(grant_option);
+        column_grant_scope.push_back(grant_scope);
     };
 
     auto add_rows = [&](const String & grantee_name,
@@ -148,16 +161,22 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
             const auto * database = element.anyDatabase() ? nullptr : &element.database;
             const auto * table = element.anyTable() ? nullptr : &element.table;
 
+            Int8 grant_scope = 0; /// exact
+            if (!element.anyDatabase() && element.anyTable() && element.wildcard)
+                grant_scope = 1; /// database_prefix
+            else if (!element.anyTable() && element.anyColumn() && element.wildcard)
+                grant_scope = 2; /// table_prefix
+
             if (element.anyColumn())
             {
                 for (const auto & access_type : access_types)
-                    add_row(grantee_name, grantee_type, access_type, element.parameter, database, table, nullptr, element.is_partial_revoke, element.grant_option);
+                    add_row(grantee_name, grantee_type, access_type, element.parameter, database, table, nullptr, element.is_partial_revoke, element.grant_option, grant_scope);
             }
             else
             {
                 for (const auto & access_type : access_types)
                     for (const auto & column : element.columns)
-                        add_row(grantee_name, grantee_type, access_type, element.parameter, database, table, &column, element.is_partial_revoke, element.grant_option);
+                        add_row(grantee_name, grantee_type, access_type, element.parameter, database, table, &column, element.is_partial_revoke, element.grant_option, grant_scope);
             }
         }
     };

hanzoai/datastore

src/Storages/System/StorageSystemGrants.cpp
@@ -39,6 +39,16 @@ ColumnsDescription StorageSystemGrants::getColumnsDescription()
             "1 — The row describes a partial revoke."
         },
         {"grant_option", std::make_shared<DataTypeUInt8>(), "Permission is granted WITH GRANT OPTION."},
+        {"grant_scope", std::make_shared<DataTypeEnum8>(
+            DataTypeEnum8::Values{
+                {"exact",            0},
+                {"database_prefix",  1},
+                {"table_prefix",     2},
+            }), "Scope type of the grant: "
+                "'exact' — exact database/table match, "
+                "'database_prefix' — database name is a wildcard prefix (e.g. db*.*), "
+                "'table_prefix' — table name is a wildcard prefix (e.g. foo.bar*)."
+        },
     };
 }
 
@@ -70,6 +80,7 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
     auto & column_column_null_map = assert_cast<ColumnNullable &>(*res_columns[column_index++]).getNullMapData();
     auto & column_is_partial_revoke = assert_cast<ColumnUInt8 &>(*res_columns[column_index++]).getData();
     auto & column_grant_option = assert_cast<ColumnUInt8 &>(*res_columns[column_index++]).getData();
+    auto & column_is_wildcard = assert_cast<ColumnUInt8 &>(*res_columns[column_index++]).getData();
 
     auto add_row = [&](const String & grantee_name,
                        AccessEntityType grantee_type,
@@ -79,7 +90,8 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
                        const String * table,
                        const String * column,
                        bool is_partial_revoke,
-                       bool grant_option)
+                       bool grant_option,
+                       bool is_wildcard)
     {
         if (grantee_type == AccessEntityType::USER)
         {
@@ -100,6 +112,7 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
 
         column_access_type.push_back(static_cast<Int16>(access_type));
         column_access_object.insertData(access_object.data(), access_object.length());
+
         if (database)
         {
             column_database.insertData(database->data(), database->length());
@@ -135,6 +148,7 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
 
         column_is_partial_revoke.push_back(is_partial_revoke);
         column_grant_option.push_back(grant_option);
+        column_is_wildcard.push_back(is_wildcard);
     };
 
     auto add_rows = [&](const String & grantee_name,
@@ -158,16 +172,18 @@ void StorageSystemGrants::fillData(MutableColumns & res_columns, ContextPtr cont
                 access_object += buf.str();
             }
 
+            const bool is_wildcard = element.wildcard;
+
             if (element.anyColumn())
             {
                 for (const auto & access_type : access_types)
-                    add_row(grantee_name, grantee_type, access_type, access_object, database, table, nullptr, element.is_partial_revoke, element.grant_option);
+                    add_row(grantee_name, grantee_type, access_type, access_object, database, table, nullptr, element.is_partial_revoke, element.grant_option, is_wildcard);
             }
             else
             {
                 for (const auto & access_type : access_types)
                     for (const auto & column : element.columns)
-                        add_row(grantee_name, grantee_type, access_type, access_object, database, table, &column, element.is_partial_revoke, element.grant_option);
+                        add_row(grantee_name, grantee_type, access_type, access_object, database, table, &column, element.is_partial_revoke, element.grant_option, is_wildcard);
             }
         }
     };