boomeer/ClickHouse

Add `aiGenerateEmbedding` tests to 03300_ai_functions
Extends the AI function test suite with `aiGenerateEmbedding`-specific
coverage: the function is registered, argument-count validation, rejecting
a non-constant `collection`, rejecting a non-constant `dimensions`, and
rejecting wrong types (signed int, string) for `dimensions`; return type
is `Array(Float32)` both with and without `dimensions`; empty input
returns zero rows without hitting the network; unknown named collection
raises `NAMED_COLLECTION_DOESNT_EXIST`; and the
`ai_function_embedding_max_batch_size` setting default is `100`.

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>

tests/queries/0_stateless/03300_ai_functions.sql
@@ -229,12 +229,66 @@ WHERE name IN (
     'ai_function_max_input_tokens_per_query',
     'ai_function_max_output_tokens_per_query',
     'ai_function_max_api_calls_per_query',
-    'ai_function_throw_on_quota_exceeded'
+    'ai_function_throw_on_quota_exceeded',
+    'ai_function_embedding_max_batch_size'
 )
 ORDER BY name;
 
 -- =============================================================================
--- 14. Re-disable the setting mid-session
+-- 14. aiGenerateEmbedding
+-- =============================================================================
+
+SELECT '-- aiGenerateEmbedding: registered';
+SELECT name FROM system.functions WHERE name = 'aiGenerateEmbedding';
+
+SELECT '-- aiGenerateEmbedding: too few arguments';
+SELECT aiGenerateEmbedding('ai_test'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+
+SELECT '-- aiGenerateEmbedding: too many arguments';
+SELECT aiGenerateEmbedding('ai_test', 'x', 256, 'extra'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+
+SELECT '-- aiGenerateEmbedding: non-constant dimensions';
+SELECT aiGenerateEmbedding('ai_test', x, toUInt64(number)) FROM (SELECT x, 0 AS number FROM _03300_input); -- { serverError ILLEGAL_COLUMN }
+
+SELECT '-- aiGenerateEmbedding: wrong type for dimensions (signed integer)';
+SELECT aiGenerateEmbedding('ai_test', x, -1) FROM _03300_input; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT '-- aiGenerateEmbedding: wrong type for dimensions (string)';
+SELECT aiGenerateEmbedding('ai_test', x, '256') FROM _03300_input; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT '-- aiGenerateEmbedding: non-constant collection';
+SELECT aiGenerateEmbedding(x, x) FROM _03300_input; -- { serverError ILLEGAL_COLUMN }
+
+SELECT '-- aiGenerateEmbedding: return type';
+DROP TABLE IF EXISTS _03300_ret_embed;
+CREATE TABLE _03300_ret_embed ENGINE = Memory AS
+    SELECT aiGenerateEmbedding('ai_test', x) AS result FROM _03300_input;
+SELECT name, type FROM system.columns
+    WHERE database = currentDatabase() AND table = '_03300_ret_embed';
+DROP TABLE IF EXISTS _03300_ret_embed;
+
+SELECT '-- aiGenerateEmbedding: return type with dimensions';
+DROP TABLE IF EXISTS _03300_ret_embed_dim;
+CREATE TABLE _03300_ret_embed_dim ENGINE = Memory AS
+    SELECT aiGenerateEmbedding('ai_test', x, 256) AS result FROM _03300_input;
+SELECT name, type FROM system.columns
+    WHERE database = currentDatabase() AND table = '_03300_ret_embed_dim';
+DROP TABLE IF EXISTS _03300_ret_embed_dim;
+
+SELECT '-- aiGenerateEmbedding: empty input executes';
+SELECT count() FROM (SELECT aiGenerateEmbedding('ai_test', x) AS result FROM _03300_input);
+
+SELECT '-- aiGenerateEmbedding: empty input with dimensions';
+SELECT count() FROM (SELECT aiGenerateEmbedding('ai_test', x, 128) AS result FROM _03300_input);
+
+SELECT '-- aiGenerateEmbedding: nonexistent named collection';
+SELECT aiGenerateEmbedding('nonexistent_collection_xyz', 'hello'); -- { serverError NAMED_COLLECTION_DOESNT_EXIST }
+
+SELECT '-- aiGenerateEmbedding: batch size setting default';
+SELECT default FROM system.settings WHERE name = 'ai_function_embedding_max_batch_size';
+
+-- =============================================================================
+-- 15. Re-disable the setting mid-session
 -- =============================================================================
 
 SET allow_experimental_ai_functions = 0;

hanzoai/datastore

tests/queries/0_stateless/03300_ai_functions.sql
@@ -229,7 +229,8 @@ WHERE name IN (
     'ai_function_max_input_tokens_per_query',
     'ai_function_max_output_tokens_per_query',
     'ai_function_max_api_calls_per_query',
-    'ai_function_throw_on_quota_exceeded'
+    'ai_function_throw_on_quota_exceeded',
+    'ai_function_embedding_max_batch_size'
 )
 ORDER BY name;
 
@@ -349,7 +350,60 @@ SELECT '-- aiTranslate: with instructions and temperature';
 SELECT count() FROM (SELECT aiTranslate('ai_credentials', x, 'French', 'keep proper nouns', 0.3) AS result FROM tab);
 
 -- =============================================================================
--- 17. Re-disable the setting mid-session
+-- 17. aiGenerateEmbedding
+-- =============================================================================
+
+SELECT '-- aiGenerateEmbedding: registered';
+SELECT name FROM system.functions WHERE name = 'aiGenerateEmbedding';
+
+SELECT '-- aiGenerateEmbedding: too few arguments';
+SELECT aiGenerateEmbedding('ai_credentials'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+
+SELECT '-- aiGenerateEmbedding: too many arguments';
+SELECT aiGenerateEmbedding('ai_credentials', 'x', 256, 'extra'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+
+SELECT '-- aiGenerateEmbedding: non-constant dimensions';
+SELECT aiGenerateEmbedding('ai_credentials', x, toUInt64(number)) FROM (SELECT x, 0 AS number FROM tab); -- { serverError ILLEGAL_COLUMN }
+
+SELECT '-- aiGenerateEmbedding: wrong type for dimensions (signed integer)';
+SELECT aiGenerateEmbedding('ai_credentials', x, -1) FROM tab; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT '-- aiGenerateEmbedding: wrong type for dimensions (string)';
+SELECT aiGenerateEmbedding('ai_credentials', x, '256') FROM tab; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT '-- aiGenerateEmbedding: non-constant collection';
+SELECT aiGenerateEmbedding(x, x) FROM tab; -- { serverError ILLEGAL_COLUMN }
+
+SELECT '-- aiGenerateEmbedding: return type';
+DROP TABLE IF EXISTS _03300_ret_embed;
+CREATE TABLE _03300_ret_embed ENGINE = Memory AS
+    SELECT aiGenerateEmbedding('ai_credentials', x) AS result FROM tab;
+SELECT name, type FROM system.columns
+    WHERE database = currentDatabase() AND table = '_03300_ret_embed';
+DROP TABLE IF EXISTS _03300_ret_embed;
+
+SELECT '-- aiGenerateEmbedding: return type with dimensions';
+DROP TABLE IF EXISTS _03300_ret_embed_dim;
+CREATE TABLE _03300_ret_embed_dim ENGINE = Memory AS
+    SELECT aiGenerateEmbedding('ai_credentials', x, 256) AS result FROM tab;
+SELECT name, type FROM system.columns
+    WHERE database = currentDatabase() AND table = '_03300_ret_embed_dim';
+DROP TABLE IF EXISTS _03300_ret_embed_dim;
+
+SELECT '-- aiGenerateEmbedding: empty input executes';
+SELECT count() FROM (SELECT aiGenerateEmbedding('ai_credentials', x) AS result FROM tab);
+
+SELECT '-- aiGenerateEmbedding: empty input with dimensions';
+SELECT count() FROM (SELECT aiGenerateEmbedding('ai_credentials', x, 128) AS result FROM tab);
+
+SELECT '-- aiGenerateEmbedding: nonexistent named collection';
+SELECT aiGenerateEmbedding('nonexistent_collection_xyz', 'hello'); -- { serverError NAMED_COLLECTION_DOESNT_EXIST }
+
+SELECT '-- aiGenerateEmbedding: batch size setting default';
+SELECT default FROM system.settings WHERE name = 'ai_function_embedding_max_batch_size';
+
+-- =============================================================================
+-- 18. Re-disable the setting mid-session
 -- =============================================================================
 
 SET allow_experimental_ai_functions = 0;