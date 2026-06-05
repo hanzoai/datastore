d840cfd0985e0e492baa2ab7c3f2ea265e96ed1d
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-06-05 18:43:31 +0000

    simplify and fix untracked memory accounting in MemoryTrackerSwitcher

--- src/Common/MemoryTrackerSwitcher.cpp
+++ src/Common/MemoryTrackerSwitcher.cpp
@@ -12,12 +12,8 @@ MemoryTrackerSwitcher::MemoryTrackerSwitcher(MemoryTracker * new_tracker)
         return;
 
     auto * thread_tracker = CurrentThread::getMemoryTracker();
-
-    prev_untracked_memory = current_thread->untracked_memory.load();
-    prev_untracked_memory_blocker_level = current_thread->untracked_memory_blocker_level;
     prev_memory_tracker_parent = thread_tracker->getParent();
 
-    current_thread->untracked_memory.store(0);
     thread_tracker->setParent(new_tracker);
 }
 
@@ -29,12 +25,7 @@ MemoryTrackerSwitcher::~MemoryTrackerSwitcher()
 
     CurrentThread::flushUntrackedMemory();
     auto * thread_tracker = CurrentThread::getMemoryTracker();
-
-    /// It is important to set untracked memory after the call of
-    /// 'setParent' because it may flush untracked memory to the wrong parent.
     thread_tracker->setParent(prev_memory_tracker_parent);
-    current_thread->untracked_memory.store(prev_untracked_memory);
-    current_thread->untracked_memory_blocker_level = prev_untracked_memory_blocker_level;
 }
 
 }
--- src/Common/MemoryTrackerSwitcher.h
+++ src/Common/MemoryTrackerSwitcher.h
@@ -12,8 +12,6 @@ struct MemoryTrackerSwitcher
 
 private:
     MemoryTracker * prev_memory_tracker_parent = nullptr;
-    Int64 prev_untracked_memory = 0;
-    VariableContext prev_untracked_memory_blocker_level = VariableContext::Max;
 };
 
 }
