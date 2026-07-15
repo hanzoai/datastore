19bde9553bfec9a8f9888345abed92181e3beec3
János Benjamin Antal <benjamin.antal@clickhouse.com>
2026-07-15 17:04:24 +0000

    Fix order

--- tests/queries/0_stateless/04412_constant_join_algorithm.sql
+++ tests/queries/0_stateless/04412_constant_join_algorithm.sql
@@ -11,15 +11,15 @@ INSERT INTO constant_join_algorithm_t2 VALUES (2), (3);
 
 -- Constant joins use ConstantJoin independently of join_algorithm.
 -- { echo on }
-SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 SETTINGS join_algorithm = 'full_sorting_merge';
-SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 SETTINGS join_algorithm = 'partial_merge';
-SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 SETTINGS join_algorithm = 'auto';
+SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 ORDER BY ALL SETTINGS join_algorithm = 'full_sorting_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 ORDER BY ALL SETTINGS join_algorithm = 'partial_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON 1 = 1 ORDER BY ALL SETTINGS join_algorithm = 'auto';
 
-SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON NULL SETTINGS join_algorithm = 'full_sorting_merge';
-SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON NULL SETTINGS join_algorithm = 'partial_merge';
-SELECT * FROM constant_join_algorithm_t1 AS t1 LEFT JOIN constant_join_algorithm_t2 AS t2 ON NULL SETTINGS join_algorithm = 'partial_merge';
-SELECT * FROM constant_join_algorithm_t1 AS t1 RIGHT JOIN constant_join_algorithm_t2 AS t2 ON NULL SETTINGS join_algorithm = 'auto';
-SELECT * FROM constant_join_algorithm_t1 AS t1 FULL JOIN constant_join_algorithm_t2 AS t2 ON NULL SETTINGS join_algorithm = 'partial_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON NULL ORDER BY ALL SETTINGS join_algorithm = 'full_sorting_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 JOIN constant_join_algorithm_t2 AS t2 ON NULL ORDER BY ALL SETTINGS join_algorithm = 'partial_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 LEFT JOIN constant_join_algorithm_t2 AS t2 ON NULL ORDER BY ALL SETTINGS join_algorithm = 'partial_merge';
+SELECT * FROM constant_join_algorithm_t1 AS t1 RIGHT JOIN constant_join_algorithm_t2 AS t2 ON NULL ORDER BY ALL SETTINGS join_algorithm = 'auto';
+SELECT * FROM constant_join_algorithm_t1 AS t1 FULL JOIN constant_join_algorithm_t2 AS t2 ON NULL ORDER BY ALL SETTINGS join_algorithm = 'partial_merge';
 
 DROP TABLE IF EXISTS constant_join_algorithm_t1;
 DROP TABLE IF EXISTS constant_join_algorithm_t2;
