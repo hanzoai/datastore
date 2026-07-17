ec88d2bbc9f9669e00ce5ac9163bb6b9f366149d
Sayantanu Dey <sayantanu.dey@gmail.com>
2026-07-18 00:55:49 +0530

    Fix test case to respect build flags

--- src/IO/tests/gtest_choose_http_compression.cpp
+++ src/IO/tests/gtest_choose_http_compression.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "config.h"
+
 #include <IO/CompressionMethod.h>
 
 using namespace DB;
@@ -7,7 +9,11 @@ using namespace DB;
 TEST(ChooseHTTPCompressionMethod, PrefersServerOrder)
 {
     EXPECT_EQ(chooseHTTPCompressionMethod("zstd, gzip, br"), CompressionMethod::Zstd);
+#if USE_BROTLI
     EXPECT_EQ(chooseHTTPCompressionMethod("br, gzip"), CompressionMethod::Brotli);
+#else
+    EXPECT_EQ(chooseHTTPCompressionMethod("br, gzip"), CompressionMethod::Gzip);
+#endif
     EXPECT_EQ(chooseHTTPCompressionMethod("gzip"), CompressionMethod::Gzip);
 }
 
@@ -23,6 +29,16 @@ TEST(ChooseHTTPCompressionMethod, HandlesDefaultQValue)
 {
     EXPECT_EQ(chooseHTTPCompressionMethod("gzip;q=0.5, zstd"), CompressionMethod::Zstd);
     EXPECT_EQ(chooseHTTPCompressionMethod("zstd;q=0.1, br;q=1"), CompressionMethod::Zstd);
+#if USE_BROTLI
+    EXPECT_EQ(chooseHTTPCompressionMethod("br;q=0.8, gzip"), CompressionMethod::Brotli);
+#else
+    EXPECT_EQ(chooseHTTPCompressionMethod("br;q=0.8, gzip"), CompressionMethod::Gzip);
+#endif
+#if USE_BZIP2
+    EXPECT_EQ(chooseHTTPCompressionMethod("bz2;q=0.8, gzip"), CompressionMethod::Bzip2);
+#else
+    EXPECT_EQ(chooseHTTPCompressionMethod("bz2;q=0.8, gzip"), CompressionMethod::Gzip);
+#endif
 }
 
 TEST(ChooseHTTPCompressionMethod, NoSubstringFalsePositives)
